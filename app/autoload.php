75b0278478f3118d75289542574906281f4fe456
Davide Bellettini <davide.bellettini@gmail.com>
2012-03-08 23:45:51 +0100

    Forgot unstaged files

---
+++ app/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+$loader = new UniversalClassLoader();
+
+$loader->registerNamespace('Symfony', __DIR__ . '/../vendor/symfony/symfony/src');
+
+$loader->registerNamespaceFallbacks(array(
+        __DIR__.'/../src'
+));
+
+$loader->registerPrefixFallbacks(array(
+        __DIR__.'/../framework',
+        __DIR__.'/../universibo',
+));
+
+$loader->register();
---
+++ src/UniversiBO/CoreBundle/UniversiBOCoreBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace UniversiBO\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @license GPL v2 or later
+ * @author Davide Bellettini <davide.bellettini@gmail.com>
+ */
+class UniversiBOCoreBundle extends Bundle
+{
+}
