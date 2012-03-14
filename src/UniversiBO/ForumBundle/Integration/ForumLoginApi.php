eff89b1132cc798e7eb8c57a67c7c88728fc177f
Davide Bellettini <davide.bellettini@gmail.com>
2012-03-14 22:41:00 +0100

    Splitting ForumApi Interface

---
+++ src/UniversiBO/ForumBundle/Integration/ForumLoginApi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UniversiBO\ForumBundle\Integration;
+
+use UniversiBO\Legacy\App\User;
+
+/**
+ * @author Davide Bellettini
+ */
+interface ForumLoginApi
+{
+    public function getSidForUri();
+    public function getOnlySid();
+    public function getPath();
+    public function login(User $user);
+    public function logout();
+}
