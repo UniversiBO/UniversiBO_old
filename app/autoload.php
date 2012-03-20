5e00c6cd9d42ee06cd4c5ff3034b21b8797d2e6a
Davide Bellettini <davide.bellettini@gmail.com>
2012-03-21 00:06:19 +0100

    Fixing autoload

--- app/autoload.php
+++ app/autoload.php
@@ -6,19 +6,19 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-        'Symfony\\Bundle\\AsseticBundle'          => __DIR__.'/../vendor/symfony/assetic-bundle',
-        'Symfony'          => __DIR__.'/../vendor/symfony/symfony/src',
+        'Symfony\\Bundle\\AsseticBundle'       => __DIR__.'/../vendor/symfony/assetic-bundle',
+        'Symfony'                              => __DIR__.'/../vendor/symfony/symfony/src',
         'Sensio\\Bundle\\FrameworkExtraBundle' => __DIR__.'/../vendor/sensio/framework-extra-bundle',
         'Sensio\\Bundle\\GeneratorBundle'      => __DIR__.'/../vendor/sensio/generator-bundle',
-
-        //        'JMS'              => __DIR__.'/../vendor/jms/bundles',
-        'Doctrine\\Common' => __DIR__.'/../vendor/doctrine/common/lib',
-        'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine/dbal/lib',
-        'Doctrine\\ORM'   => __DIR__.'/../vendor/doctrine/orm/lib',
-        'Monolog'          => __DIR__.'/../vendor/monolog/monolog/src',
-        'Assetic'          => __DIR__.'/../vendor/kriswallsmith/assetic/src',
-
-        //        'Metadata'         => __DIR__.'/../vendor/metadata/src',
+        'CG'                                   => __DIR__.'/../vendor/jms/cg/src',
+        'JMS\\AopBundle'                       => __DIR__.'/../vendor/jms/aop-bundle',
+        'JMS\\SecurityExtraBundle'             => __DIR__.'/../vendor/jms/security-extra-bundle',
+        'Doctrine\\Common'                     => __DIR__.'/../vendor/doctrine/common/lib',
+        'Doctrine\\DBAL'                       => __DIR__.'/../vendor/doctrine/dbal/lib',
+        'Doctrine\\ORM'                        => __DIR__.'/../vendor/doctrine/orm/lib',
+        'Monolog'                              => __DIR__.'/../vendor/monolog/monolog/src',
+        'Assetic'                              => __DIR__.'/../vendor/kriswallsmith/assetic/src',
+        'Metadata'                             => __DIR__.'/../vendor/jms/metadata/src',
 ));
 $loader->registerPrefixes(array(
 //        'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
