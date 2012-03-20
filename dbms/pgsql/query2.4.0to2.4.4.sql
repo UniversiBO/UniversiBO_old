e5cc71d44246a6abb856932af09abc7df7dab4fe
Davide Bellettini <davide.bellettini@gmail.com>
2012-03-20 10:52:58 +0100

    btree

--- dbms/pgsql/query2.4.0to2.4.4.sql
+++ dbms/pgsql/query2.4.0to2.4.4.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "utente" ALTER COLUMN "password" TYPE CHARACTER VARYING (64);
 ALTER TABLE "utente" ADD "algoritmo" CHARACTER VARYING (8) DEFAULT '' NOT NULL;
 ALTER TABLE "utente" ADD "salt"  CHARACTER VARYING (8) DEFAULT '' NOT NULL;
+CREATE INDEX notifica_eliminata ON notifica USING btree(eliminata);
