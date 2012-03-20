be454043ba0d0eb7ef4d142904f41c975e4d8fbe
Davide Bellettini <davide.bellettini@gmail.com>
2012-03-20 23:04:43 +0100

    dead bundle in kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
                 new Symfony\Bundle\AsseticBundle\AsseticBundle(),
                 //            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
                 //            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new UniversiBO\Core\Bundle\UniversiBOCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
