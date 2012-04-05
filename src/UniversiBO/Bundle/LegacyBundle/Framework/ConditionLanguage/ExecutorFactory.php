a9ae9c1fa023f2d3e99b1181b4ac4148b9407701
Davide Bellettini <davide.bellettini@gmail.com>
2012-04-05 14:56:39 +0200

    Executor factory refactoring

--- src/UniversiBO/Bundle/LegacyBundle/Framework/ConditionLanguage/ExecutorFactory.php
+++ src/UniversiBO/Bundle/LegacyBundle/Framework/ConditionLanguage/ExecutorFactory.php
@@ -1,39 +1,21 @@
 <?php
 namespace UniversiBO\Bundle\LegacyBundle\Framework\ConditionLanguage;
 
-
 class ExecutorFactory
 {
-	static $exs = array();
+    private static $factory;
 	
-	function init($listaExecutor)
+	public static function init($listaExecutor)
 	{
-		self::$exs = $listaExecutor;  //TODO conf in un file? al momento il visitor chiama erroneamente dispatch in maniera statica
+	    self::$factory = new ExecutorFactorySf2();
+	    
+	    foreach($listaExecutor as $executor => $impl) {
+	        self::$factory->register($executor, $impl);
+	    }
 	}
 	
-	function dispatch($executor, $arg)
+	public static function dispatch($executor, $arg)
 	{
-		if(!array_key_exists(strtolower($executor),self::$exs)) throw new Exception(); //TODO voglio veramente usare le exception???
-		
-		if(is_array($arg) && !array_key_exists('input',$arg)) $arg['input'] = array();
-		
-		$ret = call_user_func(array(self::$exs[strtolower($executor)],'run'),$arg);
-
-		if (is_array($arg) && array_key_exists('outMask',$arg))
-		{
-			if(($tot = count($arg['outMask'])) > 1) 
-			{	
-				foreach($ret as $r)
-				{
-					for ($i=0; $i<$tot; $i++)
-						$l[$arg['outMask'][$i]] = $r[$i];
-					//var_dump($l);
-					$s[]=$l;
-				}
-				return $s;
-			}
-		}
-		//var_dump($ret);
-		return $ret;
+	    return self::$factory->dispatch($executor, $arg);
 	}
 }
