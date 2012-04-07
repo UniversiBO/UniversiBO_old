c8d75993b9a5d51399a640d498b98c2931aa6f83
Davide Bellettini <davide.bellettini@gmail.com>
2012-04-07 14:22:12 +0200

    Composer self-update

--- src/UniversiBO/Bundle/LegacyBundle/Tests/Selenium/ShowAllFilesStudentiTest.php
+++ src/UniversiBO/Bundle/LegacyBundle/Tests/Selenium/ShowAllFilesStudentiTest.php
@@ -8,28 +8,25 @@ class ShowAllFilesStudentiTest extends UniversiBOSeleniumTestCase
         parent::setUp();
     }
 
-    public function testOrder0()
-    {
-       $this->testOrder(0, 'nome');
-    }
-    
-    public function testOrder1()
-    {
-        $this->testOrder(1, 'data di inserimento');
-    }
-    
-    public function testOrder2()
-    {
-        $this->testOrder(2, 'voto medio');
-    }
-    
-    private function testOrder($order, $orderSentence)
+    /**
+     * @dataProvider provider
+     */
+    public function testOrder($order, $orderSentence)
     {
         $this->login('brain');
         $this->openCommand('ShowAllFilesStudenti', '&order='.$order);
         $this->assertSentences(array(
-        		'Tutti i Files Studenti presenti su UniversiBO',
-        		'ordinati per '.$orderSentence
+                'Tutti i Files Studenti presenti su UniversiBO',
+                'ordinati per '.$orderSentence
         ));
+    }
+
+    public function provider()
+    {
+        return array (
+                array(0, 'nome'),
+                array(1, 'data di inserimento'),
+                array(2, 'voto medio'),
+        );
     }
 }
