342069d5908e6e3668bcb19f7bb974e8f491ee9c
Davide Bellettini <davide.bellettini@gmail.com>
2012-04-18 16:38:21 +0200

    Batch rename stub

---
+++ src/UniversiBO/Bundle/LegacyBundle/Command/ScriptBatchRename.php
@@ -0,0 +1,27 @@
+<?php
+namespace UniversiBO\Bundle\LegacyBundle\Command;
+
+use UniversiBO\Bundle\LegacyBundle\Entity\DBUserRepository;
+
+use \DB;
+use \Error;
+use UniversiBO\Bundle\LegacyBundle\App\Constants;
+use UniversiBO\Bundle\LegacyBundle\App\ForumApi;
+use UniversiBO\Bundle\LegacyBundle\Entity\User;
+use UniversiBO\Bundle\LegacyBundle\App\UniversiboCommand;
+
+/**
+ * @author Davide Bellettini
+ * @license GPL v2 or later, {@link http://www.opensource.org/licenses/gpl-license.php}
+ */
+class ScriptBatchRename extends UniversiboCommand
+{
+	function execute()
+	{
+		$fc = $this->getFrontController();
+		$db = $fc->getDbConnection('main');
+		
+		
+		$repo = new DBUserRepository($db);
+	}
+}
