fe469c5948ba23b424e7f8a4737edf96a6e3f2d0
Davide Bellettini <davide.bellettini@gmail.com>
2012-05-01 19:27:06 +0200

    Fixing autoload

--- app/autoload.php
+++ app/autoload.php
@@ -22,6 +22,7 @@ $loader->registerNamespaces(array(
         'Assetic'                              => __DIR__.'/../vendor/kriswallsmith/assetic/src',
         'Metadata'                             => __DIR__.'/../vendor/jms/metadata/src',
         'Zend\\Feed'                           => __DIR__.'/../vendor/brikou/zend_feed',
+        'Zend\\Loader'                         => __DIR__.'/../vendor/brikou/zend_loader',
 ));
 $loader->registerPrefixes(array(
 //        'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
