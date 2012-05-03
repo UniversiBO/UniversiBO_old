0df6d8fae1f7cb93c77cd190b06ea79a8c6ff5be
Davide Bellettini <davide.bellettini@gmail.com>
2012-05-03 11:18:06 +0200

    Formatting commit

--- src/UniversiBO/Bundle/LegacyBundle/App/ProgrammazioneDidattica/ProgrammazioneDidatticaDataRetriever.php
+++ src/UniversiBO/Bundle/LegacyBundle/App/ProgrammazioneDidattica/ProgrammazioneDidatticaDataRetriever.php
@@ -1,6 +1,5 @@
 <?php
 namespace UniversiBO\Bundle\LegacyBundle\App\ProgrammazioneDidattica;
-
 /**
  * ProgrammazioneDidatticaDataRetriever
  *
@@ -13,61 +12,60 @@ namespace UniversiBO\Bundle\LegacyBundle\App\ProgrammazioneDidattica;
  * @license GPL, @link http://www.opensource.org/licenses/gpl-license.php
  * @copyright CopyLeft UniversiBO 2001-2004
  */
-
 interface ProgrammazioneDidatticaDataRetriever
 {
-	/**
-	 * @return Facolta[]
-	 */
-	public function getFacoltaList();
-	
-	/**
-	 * @param string $codFac
-	 * @return Facolta
-	 */
-	public function getFacolta($codFac);
-	
-	/**
-	 * @param string $codFac
-	 * @return Corso[]
-	 */
-	public function getCorsoListFacolta($codFac);
-	
-	/**
-	 * @param string $codCorso
-	 * @return Corso 
-	 */
-	public function getCorso($codCorso);
-	
-	/**
-	 * @param string $codMateria
-	 * @return  Materia
-	 */
-	public function getMateria($codMateria);
-	
-	/**
-	 * @param string $codDoc
-	 * @return  Docente 
-	 */
-	public function getDocente($codDoc);
-	
-	/**
-	 * @param string $codCorso
-	 * @param int $annoAccademico
-	 * @return AttivitaDidattica[] 
-	 */
-	public function getAttivitaDidatticaPadreCorso($codCorso, $annoAccademico);
-	
-	/**
-	 * @param string $codCorso
-	 * @param int $annoAccademico
-	 * @return AttivitaDidattica[] 
-	 */
-	public function getAttivitaDidatticaCorso($codCorso, $annoAccademico);
-	
-	/**
-	 * @param AttivitaDidattica $attivitaPadre
-	 * @return AttivitaDidattica[] 
-	 */
-	public function getSdoppiamentiAttivitaDidattica($attivitaPadre);
+    /**
+     * @return Facolta[]
+     */
+    public function getFacoltaList();
+
+    /**
+     * @param string $codFac
+     * @return Facolta
+     */
+    public function getFacolta($codFac);
+
+    /**
+     * @param string $codFac
+     * @return Corso[]
+     */
+    public function getCorsoListFacolta($codFac);
+
+    /**
+     * @param string $codCorso
+     * @return Corso 
+     */
+    public function getCorso($codCorso);
+
+    /**
+     * @param string $codMateria
+     * @return  Materia
+     */
+    public function getMateria($codMateria);
+
+    /**
+     * @param string $codDoc
+     * @return  Docente 
+     */
+    public function getDocente($codDoc);
+
+    /**
+     * @param string $codCorso
+     * @param int $annoAccademico
+     * @return AttivitaDidattica[] 
+     */
+    public function getAttivitaDidatticaPadreCorso($codCorso, $annoAccademico);
+
+    /**
+     * @param string $codCorso
+     * @param int $annoAccademico
+     * @return AttivitaDidattica[] 
+     */
+    public function getAttivitaDidatticaCorso($codCorso, $annoAccademico);
+
+    /**
+     * @param AttivitaDidattica $attivitaPadre
+     * @return AttivitaDidattica[] 
+     */
+    public function getSdoppiamentiAttivitaDidattica($attivitaPadre);
 }
