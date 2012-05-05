642a60df58639cc9a951c13e05f659bac6f620a1
Davide Bellettini <davide.bellettini@gmail.com>
2012-05-05 14:28:43 +0200

    Deploy file for Capifony almost there

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,9 +1,10 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
+set :application, "UniversiBO"
+set :domain,      "universibo.unibo.it"
+set :deploy_to,   "/var/www/universibo"
 set :app_path,    "app"
+set :web_path,    "htmls"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :repository,  "git@github.com:UniversiBO/UniversiBO.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, `subversion` or `none`
 
@@ -14,4 +15,9 @@ role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Rails migrations will run
 
+set  :shared_files,    ["app/config/parameters.ini", "composer.lock", "config.xml", "config_cli.xml"]
+set  :shared_children, [app_path + "/logs", web_path + "/img/contacts", web_path + "/forum/images/avatars","universibo/file-universibo", "universibo/log-universibo", "vendor"]
+
+set  :update_vendors, false
+set  :use_composer,   true
 set  :keep_releases,  3
