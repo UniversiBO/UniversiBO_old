66a6f908327f5840d4689e882b9ab339a3210e7e
Davide Bellettini <davide.bellettini@gmail.com>
2012-05-16 15:00:15 +0200

    Layout improving + UserInfo sidebox

---
+++ src/UniversiBO/Bundle/WebsiteBundle/Controller/UserInfoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UniversiBO\Bundle\WebsiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class UserInfoController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
