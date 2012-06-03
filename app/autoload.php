822e7d9fe2d65d875605437f6daa62aaaac9d942
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-03 12:18:06 +0200

    Letting Composer managing autoload

--- app/autoload.php
+++ app/autoload.php
@@ -1,5 +1,4 @@
 <?php
-require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/ClassLoader/ApcUniversalClassLoader.php';
 
 use Symfony\Component\ClassLoader\ApcUniversalClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
@@ -7,57 +6,25 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 define('PHP_EXTENSION', '.php');
 define('UNIVERSIBO_ROOT', realpath(__DIR__.'/..'));
 
-$loader = new ApcUniversalClassLoader('u3_class');
-$loader->registerNamespaces(array(
-        'UniversiBO\\Bundle\\AnswersBundle'    => __DIR__.'/../vendor/universibo/universibo-answers-bundle',
-        'Symfony\\Bundle\\AsseticBundle'       => __DIR__.'/../vendor/symfony/assetic-bundle',
-        'Symfony'                              => __DIR__.'/../vendor/symfony/symfony/src',
-        'Sensio\\Bundle\\FrameworkExtraBundle' => __DIR__.'/../vendor/sensio/framework-extra-bundle',
-        'Sensio\\Bundle\\GeneratorBundle'      => __DIR__.'/../vendor/sensio/generator-bundle',
-        'CG'                                   => __DIR__.'/../vendor/jms/cg/src',
-        'JMS\\AopBundle'                       => __DIR__.'/../vendor/jms/aop-bundle',
-        'JMS\\SecurityExtraBundle'             => __DIR__.'/../vendor/jms/security-extra-bundle',
-        'JMS\\SerializerBundle'                => __DIR__.'/../vendor/jms/serializer-bundle',
-        'Doctrine\\Common'                     => __DIR__.'/../vendor/doctrine/common/lib',
-        'Doctrine\\DBAL\\Migrations'           => __DIR__.'/../vendor/doctrine/migrations/lib',
-        'Doctrine\\DBAL'                       => __DIR__.'/../vendor/doctrine/dbal/lib',
-        'Doctrine\\ORM'                        => __DIR__.'/../vendor/doctrine/orm/lib',
-        'Monolog'                              => __DIR__.'/../vendor/monolog/monolog/src',
-        'Assetic'                              => __DIR__.'/../vendor/kriswallsmith/assetic/src',
-        'Metadata'                             => __DIR__.'/../vendor/jms/metadata/src',
-        'Zend'                                 => __DIR__.'/../vendor/zendframework/zendframework/library',
-        'FOS\\CommentBundle'                   => __DIR__.'/../vendor/friendsofsymfony/comment-bundle',
-        'FOS\\OAuthServerBundle'               => __DIR__.'/../vendor/friendsofsymfony/oauth-server-bundle',
-        'FOS\\Rest'                            => __DIR__.'/../vendor/friendsofsymfony/rest',
-        'FOS\\RestBundle'                      => __DIR__.'/../vendor/friendsofsymfony/rest-bundle',
-));
-$loader->registerPrefixes(array(
-//        'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
-        'Twig_'            => __DIR__.'/../vendor/twig/twig/lib',
-));
+if (!$loader = include __DIR__.'/../vendor/autoload.php') {
+    $nl = PHP_SAPI === 'cli' ? PHP_EOL : '<br />';
+    echo "$nl$nl";
+    die('You must set up the project dependencies.'.$nl.
+        'Run the following commands in '.dirname(__DIR__).':'.$nl.$nl.
+        'curl -s http://getcomposer.org/installer | php'.$nl.
+        'php composer.phar install'.$nl);
+}
 
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $loader->registerPrefixFallbacks(array(
-            __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs',
-    ));
+    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
 
-$loader->registerPrefixFallbacks(array(
-        __DIR__.'/../framework',
-));
+$loader->add('',__DIR__.'/../framework');
+$loader->add('',__DIR__.'/../src');
 
-$loader->registerNamespaceFallbacks(array(
-        __DIR__.'/../src',
-));
-$loader->register();
-
-AnnotationRegistry::registerLoader(function($class) use ($loader) {
-    $loader->loadClass($class);
-    return class_exists($class, false);
-});
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
 
 // Swiftmailer needs a special autoloader to allow
