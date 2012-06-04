da4f14ec6fd1b0847537f5eed418e7a6eb29bf28
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-04 11:39:07 +0200

    Improving ShowCanaleTest

--- src/UniversiBO/Bundle/LegacyBundle/Tests/Selenium/ShowCanaleTest.php
+++ src/UniversiBO/Bundle/LegacyBundle/Tests/Selenium/ShowCanaleTest.php
@@ -1,6 +1,5 @@
 <?php
 namespace UniversiBO\Bundle\LegacyBundle\Tests\Selenium;
-
 class ShowCanaleTest extends UniversiBOSeleniumTestCase
 {
     protected function setUp()
@@ -10,7 +9,11 @@ class ShowCanaleTest extends UniversiBOSeleniumTestCase
 
     public function testSimple()
     {
+        $this->login('brain');
         $this->open('/v2.php?do=ShowCanale&id_canale=2219');
-        $this->assertSentence('Area Laureati');
+        $this->assertSentences(
+                        array('Area Laureati', 'News', 'Scrivi nuova notizia',
+                                'Mostra tutte le news', 'Files',
+                                'Invia un nuovo file', 'Gestione file'));
     }
 }
