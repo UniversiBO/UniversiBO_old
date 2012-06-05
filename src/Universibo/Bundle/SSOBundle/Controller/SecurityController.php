992d05830384bd2a5085ecf3504c8fd504022aef
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-05 17:42:55 +0200

    Improving Shibboleth integration

--- src/Universibo/Bundle/SSOBundle/Controller/SecurityController.php
+++ src/Universibo/Bundle/SSOBundle/Controller/SecurityController.php
@@ -1,32 +1,35 @@
 <?php
-
 namespace Universibo\Bundle\SSOBundle\Controller;
+use Universibo\Bundle\SSOBundle\Service\ShibbolethService;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * @author Davide Bellettini <davide.bellettini@gmail.com>
  */
-class SecurityController
+class SecurityController extends ContainerAware
 {
-    /**
-     * @var EngineInterface
-     */
-    private $templating;
-
-    /**
-     * Class constructor
-     * @param  $templating
-     */
-    public function __construct(EngineInterface $templating)
-    {
-        $this->templating = $templating;
-    }
-
     public function loginAction()
     {
         return new Response('Please configure mod_shib2');
+    }
+
+    public function logoutAction()
+    {
+        $this->container->get('request')->getSession()->invalidate();
+        $this->container->get('security.context')->setToken(null);
+        
+        $this->container->get('universibo_sso.service.shibboleth')->destroyCookies();
+
+        $url = '/bundles/universibosso/images/greencheck.gif';
+        return new RedirectResponse($url);
     }
 }
---
+++ src/Universibo/Bundle/SSOBundle/Service/ShibbolethService.php
@@ -0,0 +1,13 @@
+<?php
+namespace Universibo\Bundle\SSOBundle\Service;
+
+class ShibbolethService
+{
+    public function destroyCookies()
+    {
+        $past = time() - 3600;
+        foreach ($_COOKIE as $key => $value) {
+        	setcookie($key, $value, $past, '/');
+        }
+    }
+}
