5f44e53811b82340909859ee1107735268a8617c
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-05 10:39:46 +0200

    Privacy Controller stub

---
+++ src/UniversiBO/Bundle/WebsiteBundle/Controller/PrivacyController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace UniversiBO\Bundle\WebsiteBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("/privacy")
+ */
+class PrivacyController extends Controller
+{
+    /**
+     * @Route("",name="privacy")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/accept",name="privacy_accept")
+     * @Method("POST")
+     */
+    public function acceptAction()
+    {
+    }
+}
