dc1ddeaceab36e99440d6d5fc5edca25c926791d
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-07 17:37:51 +0200

    enhancing entity tests

---
+++ src/Universibo/Bundle/LegacyBundle/Tests/Entity/UniversiBOEntityTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Universibo\Bundle\LegacyBundle\Tests\Entity;
+
+use Universibo\Bundle\LegacyBundle\Entity\User;
+
+abstract class UniversiBOEntityTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param object $object
+     * @param string $name
+     * @param mixed $value
+     */
+    protected function autoTestAccessor($object, $name, $value)
+    {
+        $setter = 'set'.ucfirst($name);
+        $getter = 'get'.ucfirst($name);
+        
+        $object->{$setter}($value);
+        
+        $this->assertEquals($value, $object->{$getter}(), 'getter value');
+    }
+}
--- src/Universibo/Bundle/LegacyBundle/Tests/Entity/UserTest.php
+++ src/Universibo/Bundle/LegacyBundle/Tests/Entity/UserTest.php
@@ -3,7 +3,7 @@ namespace Universibo\Bundle\LegacyBundle\Tests\Entity;
 
 use Universibo\Bundle\LegacyBundle\Entity\User;
 
-class UserTest extends \PHPUnit_Framework_TestCase
+class UserTest extends UniversiBOEntityTest
 {
     /**
      * @var User
@@ -38,5 +38,24 @@ class UserTest extends \PHPUnit_Framework_TestCase
     public function testSerialize()
     {
         $this->markTestIncomplete('Should test serialization');
+    }
+    
+    /**
+     * @dataProvider accessorDataProvider
+     * 
+     * @param string $name
+     * @param mixed $value
+     */
+    public function testAccessors($name, $value)
+    {
+        $this->autoTestAccessor($this->_user, $name, $value);
+    }
+    
+    public function accessorDataProvider()
+    {
+        return array(
+                array('username', 'world'),
+                array('username', 'ciao'),
+        );
     }
 }
