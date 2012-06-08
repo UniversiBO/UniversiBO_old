548db1070ddfb50094c1ca00b475b96ed865ca4c
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-08 09:42:46 +0200

    Fixing typo

--- src/Universibo/Bundle/WebsiteBundle/Controller/RssController.php
+++ src/Universibo/Bundle/WebsiteBundle/Controller/RssController.php
@@ -19,9 +19,9 @@ class RssController extends Controller
     public function indexAction($idCanale)
     {
         $canaleRepo = $this->get('universibo_legacy.repository.canale');
-        $canale = $canaleRepo->find($idCanale);
+        $channel = $canaleRepo->find($idCanale);
 
-        if (!$canale instanceof Canale) {
+        if (!$channel instanceof Canale) {
             throw $this->createNotFoundException('Canale not found');
         }
 
