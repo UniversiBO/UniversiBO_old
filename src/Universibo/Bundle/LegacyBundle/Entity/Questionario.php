297eb8bb11692e8c4b1e5848854525c2f2660565
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-09 23:40:18 +0200

    Questionario and test

---
+++ src/Universibo/Bundle/LegacyBundle/Entity/Questionario.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Universibo\Bundle\LegacyBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Universibo\Bundle\LegacyBundle\Entity\Questionario
+ *
+ * @ORM\Table(name="questionario")
+ * @ORM\Entity
+ */
+class Questionario
+{
+    /**
+     * @var integer $idQuestionario
+     *
+     * @ORM\Column(name="id_questionario", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="questionario_id_questionario_seq", allocationSize="1", initialValue="1")
+     */
+    private $idQuestionario;
+
+    /**
+     * @var integer $data
+     *
+     * @ORM\Column(name="data", type="integer", nullable=false)
+     */
+    private $data;
+
+    /**
+     * @var string $nome
+     *
+     * @ORM\Column(name="nome", type="string", length=50, nullable=false)
+     */
+    private $nome;
+
+    /**
+     * @var string $cognome
+     *
+     * @ORM\Column(name="cognome", type="string", length=50, nullable=false)
+     */
+    private $cognome;
+
+    /**
+     * @var string $mail
+     *
+     * @ORM\Column(name="mail", type="string", length=50, nullable=false)
+     */
+    private $mail;
+
+    /**
+     * @var string $telefono
+     *
+     * @ORM\Column(name="telefono", type="string", length=50, nullable=false)
+     */
+    private $telefono;
+
+    /**
+     * @var smallint $tempoDisp
+     *
+     * @ORM\Column(name="tempo_disp", type="smallint", nullable=false)
+     */
+    private $tempoDisp;
+
+    /**
+     * @var smallint $tempoInternet
+     *
+     * @ORM\Column(name="tempo_internet", type="smallint", nullable=false)
+     */
+    private $tempoInternet;
+
+    /**
+     * @var string $attivOffline
+     *
+     * @ORM\Column(name="attiv_offline", type="string", nullable=false)
+     */
+    private $attivOffline;
+
+    /**
+     * @var string $attivModeratore
+     *
+     * @ORM\Column(name="attiv_moderatore", type="string", nullable=false)
+     */
+    private $attivModeratore;
+
+    /**
+     * @var string $attivContenuti
+     *
+     * @ORM\Column(name="attiv_contenuti", type="string", nullable=false)
+     */
+    private $attivContenuti;
+
+    /**
+     * @var string $attivTest
+     *
+     * @ORM\Column(name="attiv_test", type="string", nullable=false)
+     */
+    private $attivTest;
+
+    /**
+     * @var string $attivGrafica
+     *
+     * @ORM\Column(name="attiv_grafica", type="string", nullable=false)
+     */
+    private $attivGrafica;
+
+    /**
+     * @var string $attivProg
+     *
+     * @ORM\Column(name="attiv_prog", type="string", nullable=false)
+     */
+    private $attivProg;
+
+    /**
+     * @var text $altro
+     *
+     * @ORM\Column(name="altro", type="text", nullable=false)
+     */
+    private $altro;
+
+    /**
+     * @var integer $idUtente
+     *
+     * @ORM\Column(name="id_utente", type="integer", nullable=false)
+     */
+    private $idUtente;
+
+    /**
+     * @var string $cdl
+     *
+     * @ORM\Column(name="cdl", type="string", length=50, nullable=false)
+     */
+    private $cdl;
+
+
+}
---
+++ src/Universibo/Bundle/LegacyBundle/Tests/Entity/QuestionarioTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace Universibo\Bundle\LegacyBundle\Tests\Entity;
+
+use Universibo\Bundle\LegacyBundle\Entity\Questionario;
+
+use Universibo\Bundle\LegacyBundle\Entity\User;
+
+class QuestionarioTest extends UniversiBOEntityTest
+{
+    /**
+     * @var Questionario
+     */
+    private $questionario;
+
+    protected function setUp()
+    {
+        $this->questionario = new Questionario();
+    }
+
+
+    /**
+     * @dataProvider accessorDataProvider
+     *
+     * @param string $name
+     * @param mixed  $value
+     */
+    public function testAccessors($name, $value)
+    {
+        $this->autoTestAccessor($this->questionario, $name, $value, true);
+    }
+
+    public function accessorDataProvider()
+    {
+        return array(
+                array('id', rand()),
+                array('data', rand()),
+                array('nome', 'Mario'),
+                array('cognome', 'Rossi'),
+                array('mail', 'hello@example.com'),
+                array('telefono', '3401234567'),
+                array('tempoDisponibile', 42),
+                array('tempoInternet', 42),
+                array('attivitaOffline', 'S'),
+                array('attivitaModeratore', 'S'),
+                array('attivitaContenuti', 'S'),
+                array('attivitaTest', 'S'),
+                array('attivitaGrafica', 'S'),
+                array('attivitaProgettazione', 'S'),
+                array('altro', 'Hello '.rand()),
+                array('idUtente', 43),
+                array('cdl', 'gestionale')
+        );
+    }
+}
+
