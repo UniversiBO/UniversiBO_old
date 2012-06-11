f9fd66e944e305a4fe9a958275316c55bf7344c9
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-12 00:39:58 +0200

    ShowStatistiche migrated to Doctrine DBAL (see #12)

--- src/Universibo/Bundle/LegacyBundle/Command/ShowStatistiche.php
+++ src/Universibo/Bundle/LegacyBundle/Command/ShowStatistiche.php
@@ -8,7 +8,7 @@ class ShowStatistiche extends UniversiboCommand
 {
     public function execute()
     {
-        $frontcontroller = &$this->getFrontController();
+        $frontcontroller = $this->getFrontController();
         $template = $frontcontroller->getTemplateEngine();
 
         $krono = $frontcontroller->getKrono();
@@ -22,45 +22,33 @@ class ShowStatistiche extends UniversiboCommand
                             'file' => __FILE__, 'line' => __LINE__));
         }
 
-        $db = $frontcontroller->getDbConnection('main');
+        $db = $this->getContainer()->get('doctrine.dbal.default_connection');
+        
         $query = 'SELECT file_inseriti_giorno.mese, file_inseriti_giorno.anno, sum(file_inseriti_giorno.totale_file) AS file_mese
         FROM file_inseriti_giorno
         GROUP BY file_inseriti_giorno.anno, file_inseriti_giorno.mese
         ORDER BY file_inseriti_giorno.anno DESC, file_inseriti_giorno.mese DESC;';
         //			var_dump($query,$user); die;
-        $res = $db->query($query);
-        if (DB::isError($res))
-            Error::throwError(_ERROR_CRITICAL,
-                    array('msg' => DB::errorMessage($res), 'file' => __FILE__,
-                            'line' => __LINE__));
-        $rows = $res->numRows();
-        //			echo $rows . " righe coinvolte\n";
+        $res = $db->executeQuery($query);
+        
         $listaFilePerMese = array();
-        while ($res->fetchInto($row)) {
+        while (false !== ($row = $res->fetch())) {
             $listaFilePerMese[] = array('mese' => $row[0], 'anno' => $row[1],
                     'somma' => $row[2]);
         }
 
         $query = 'SELECT giorno, mese,anno, iscritti FROM studenti_iscritti ORDER BY anno DESC, mese DESC, giorno DESC;';
         //			var_dump($query,$user); die;
-        $res = $db->query($query);
-        if (DB::isError($res))
-            Error::throwError(_ERROR_CRITICAL,
-                    array('msg' => DB::errorMessage($res), 'file' => __FILE__,
-                            'line' => __LINE__));
-        $rows = $res->numRows();
-        //			echo $rows . " righe coinvolte\n";
+        $res = $db->executeQuery($query);
+        
         $listaUtentiPerMese = array();
-        while ($res->fetchInto($row)) {
+        while (false !== ($row = $res->fetch())) {
             $listaUtentiPerMese[] = array('mese' => $row[1], 'anno' => $row[2],
                     'giorno' => $row[0], 'iscritti' => $row[3]);
         }
 
-        $template
-                ->assign('ShowStatistiche_elencoUtentiPerMese',
-                        $listaUtentiPerMese);
-        $template
-                ->assign('ShowStatistiche_elencoFilePerMese', $listaFilePerMese);
+        $template->assign('ShowStatistiche_elencoUtentiPerMese', $listaUtentiPerMese);
+        $template->assign('ShowStatistiche_elencoFilePerMese', $listaFilePerMese);
         $template->assign('ShowStatistiche_titolo', 'Statistiche');
     }
 }
