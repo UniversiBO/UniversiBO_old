34f102e90de2e022f616730447316d85914571c6
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-13 20:40:06 +0200

    Fixing typo in comment

--- src/Universibo/Bundle/LegacyBundle/Entity/InteractiveCommand/StepLog.php
+++ src/Universibo/Bundle/LegacyBundle/Entity/InteractiveCommand/StepLog.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 class StepLog
 {
     /**
-     * @var integer $idStep
+     * @var integer $id
      *
      * @ORM\Column(name="id_step", type="integer", nullable=false)
      * @ORM\Id
