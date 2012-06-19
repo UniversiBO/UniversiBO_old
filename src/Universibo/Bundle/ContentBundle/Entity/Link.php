284821465b9cce733732764ee9f5c4df09990d9a
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-19 17:37:01 +0200

    Refactoring Link entity

---
+++ src/Universibo/Bundle/ContentBundle/Entity/Link.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Universibo\Bundle\ContentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Universibo\Bundle\ContentBundle\Entity\Link
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Universibo\Bundle\ContentBundle\Entity\LinkRepository")
+ */
+class Link
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $uri
+     *
+     * @ORM\Column(name="uri", type="string", length=255)
+     */
+    private $uri;
+
+    /**
+     * @var string $label
+     *
+     * @ORM\Column(name="label", type="string", length=100)
+     */
+    private $label;
+
+    /**
+     * @var text $description
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set uri
+     *
+     * @param string $uri
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+    }
+
+    /**
+     * Get uri
+     *
+     * @return string 
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string 
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Set description
+     *
+     * @param text $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Get description
+     *
+     * @return text 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
