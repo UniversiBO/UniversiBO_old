4c9913b154ebd4e6200a630db3734489968454c5
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-19 10:04:08 +0200

    Updating fixtures

--- src/Universibo/Bundle/CoreBundle/DataFixtures/ORM/LoadGroupData.php
+++ src/Universibo/Bundle/CoreBundle/DataFixtures/ORM/LoadGroupData.php
@@ -15,12 +15,20 @@ class LoadGroupData implements FixtureInterface
         $admin->addRole('ROLE_ADMIN');
         $manager->persist($admin);
         
-        $students = new Group('Students');
-        $students->addRole('ROLE_USER');
-        $manager->persist($students);
+        $collab = new Group('Collaborators');
+        $collab->addRole('ROLE_USER');
+        $manager->persist($collab);
+        
+        $professors = new Group('Professors');
+        $professors->addRole('ROLE_USER');
+        $manager->persist($professors);
         
         $staff = new Group('Staff');
         $staff->addRole('ROLE_USER');
+        $manager->persist($staff);
+        
+        $students = new Group('Students');
+        $students->addRole('ROLE_USER');
         $manager->persist($students);
         
         $manager->flush();
