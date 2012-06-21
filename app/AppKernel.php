42c904e2a400ef1afa9447a557a2897293acd723
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-21 17:00:34 +0200

    Adding KnpPaginatorBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel {
             //new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             //new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Universibo\Bundle\WebsiteBundle\UniversiboWebsiteBundle(),
             new Universibo\Bundle\ForumBundle\UniversiboForumBundle(),
             new Universibo\Bundle\SSOBundle\UniversiboSSOBundle(),
