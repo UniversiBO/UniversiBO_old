694168f55a9e1a5560f1300a3975d982cc69a19a
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-21 16:10:30 +0200

    Adding Twig SecurityExtension close #101

---
+++ src/Universibo/Bundle/CoreBundle/Twig/SecurityExtension.php
@@ -0,0 +1,53 @@
+<?php
+namespace Universibo\Bundle\CoreBundle\Twig;
+
+use Universibo\Bundle\CoreBundle\Security\UniversiboContext;
+
+use Universibo\Bundle\CoreBundle\Entity\ChannelRepository;
+
+use Universibo\Bundle\CoreBundle\Entity\Channel;
+
+use Universibo\Bundle\CoreBundle\Routing\ChannelRouter;
+
+class SecurityExtension extends \Twig_Extension
+{
+    /**
+     * @var UniversiboContext
+     */
+    private $context;
+    
+    /**
+     * Class constructor
+     * 
+     * @param ChannelRouter $router
+     */
+    public function __construct(UniversiboContext $context)
+    {
+        $this->context = $context;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Twig_ExtensionInterface::getName()
+     */
+    public function getName()
+    {
+        return 'universibo_core.security';
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Twig_Extension::getFunctions()
+     */
+    public function getFunctions()
+    {
+        return array (
+                'ub_is_granted' =>  new \Twig_Function_Method($this, 'isGranted'),
+        );
+    }
+    
+    public function isGranted($attributes, $object = null)
+    {
+        return $this->context->isGranted($attributes, $object);
+    }
+}
