933a5eb84cb251adaf097993326cd60503fd3d25
Davide Bellettini <davide.bellettini@gmail.com>
2012-06-21 12:22:16 +0200

    Adding Service Fixtures

---
+++ src/Universibo/Bundle/ContentBundle/DataFixtures/ORM/LoadServiceData.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Universibo\Bundle\CoreBundle\DataFixtures\ORM;
+
+use Universibo\Bundle\CoreBundle\Entity\ChannelService;
+
+use Universibo\Bundle\ContentBundle\Entity\News;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+use Universibo\Bundle\CoreBundle\Entity\User;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
+use Universibo\Bundle\DidacticsBundle\Entity\Faculty;
+
+use Universibo\Bundle\CoreBundle\Entity\Channel;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+
+/**
+ * User Fixture
+ */
+class LoadNewsData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * (non-PHPdoc)
+     * @see Doctrine\Common\DataFixtures.FixtureInterface::load()
+     */
+    public function load(ObjectManager $manager)
+    {
+        $service = new ChannelService();
+        $service->setName('files');
+        $manager->persist($service);
+        $this->addReference('files-service', $service);
+        
+        $service = new ChannelService();
+        $service->setName('news');
+        $manager->persist($service);
+        $this->addReference('news-service', $service);
+        
+        $service = new ChannelService();
+        $service->setName('links');
+        $manager->persist($service);
+        $this->addReference('links-service', $service);
+        
+        $manager->flush();
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Doctrine\Common\DataFixtures.OrderedFixtureInterface::getOrder()
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
