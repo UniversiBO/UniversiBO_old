f52f83b58eb31bb10e66b619a6e45b76d57fd468
Davide Bellettini <davide.bellettini@gmail.com>
2012-08-29 19:18:40 +0200

    fix #114 fix #115

--- app/autoload.php
+++ app/autoload.php
@@ -26,8 +26,3 @@ $loader->add('',__DIR__.'/../framework');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
-
-// Swiftmailer needs a special autoloader to allow
-// the lazy loading of the init file (which is expensive)
-require_once __DIR__.'/../vendor/swiftmailer/swiftmailer/lib/classes/Swift.php';
-Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/swiftmailer/lib/swift_init.php');
