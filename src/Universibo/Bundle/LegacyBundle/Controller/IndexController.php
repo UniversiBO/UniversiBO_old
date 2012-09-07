a019a32f3f00c7d750d49e9b4562ac5f169cfe99
Davide Bellettini <davide.bellettini@gmail.com>
2012-09-07 15:07:28 +0200

    Stubbing Legacy IndexController. See #116

---
+++ src/Universibo/Bundle/LegacyBundle/Controller/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+namespace Universibo\Bundle\LegacyBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @author Davide Bellettini <davide.bellettini@gmail.com>
+ */
+class IndexController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        $request = $this->getRequest();
+        
+        $do = $request->get('do', 'ShowHomepage');
+        return new Response($do); 
+    }
+}
