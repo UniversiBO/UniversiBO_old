808958afb0495c60877ecabbd6d00dc55220a96b
Davide Bellettini <davide.bellettini@gmail.com>
2012-09-22 01:36:07 +0200

    Removing old security listeners

--- src/Universibo/Bundle/WebsiteBundle/UniversiboWebsiteBundle.php
+++ src/Universibo/Bundle/WebsiteBundle/UniversiboWebsiteBundle.php
@@ -2,18 +2,10 @@
 
 namespace Universibo\Bundle\WebsiteBundle;
 
-use Universibo\Bundle\LegacyBundle\Auth\UniversiBOFactory;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UniversiboWebsiteBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $extension = $container->getExtension('security');
-        $extension->addSecurityListenerFactory(new UniversiBOFactory());
-    }
 }
