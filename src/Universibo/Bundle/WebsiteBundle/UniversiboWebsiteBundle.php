a791d0197d4dc02b9516361c22b7512171ba4b6e
Davide Bellettini <davide.bellettini@gmail.com>
2012-09-21 21:49:13 +0200

    Symfony 2.1 migration complete

--- src/Universibo/Bundle/WebsiteBundle/UniversiboWebsiteBundle.php
+++ src/Universibo/Bundle/WebsiteBundle/UniversiboWebsiteBundle.php
@@ -2,8 +2,18 @@
 
 namespace Universibo\Bundle\WebsiteBundle;
 
+use Universibo\Bundle\LegacyBundle\Auth\UniversiBOFactory;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UniversiboWebsiteBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+    	parent::build($container);
+    	
+    	$extension = $container->getExtension('security');
+    	$extension->addSecurityListenerFactory(new UniversiBOFactory());
+    }
 }
