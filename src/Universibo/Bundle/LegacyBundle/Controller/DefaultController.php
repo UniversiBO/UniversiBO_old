f9bb19c7f442be14e5fcc14d0deb145024f56212
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-01 15:49:11 +0200

    Wrong intend, fixed

--- src/Universibo/Bundle/LegacyBundle/Controller/DefaultController.php
+++ src/Universibo/Bundle/LegacyBundle/Controller/DefaultController.php
@@ -54,7 +54,7 @@ class DefaultController extends Controller
             case 'ShowFileInfo':
                 return $this->redirect($this->get('router')->generate('universibo_legacy_file', array('id_file' => $_GET['id_file']), true), 301);
             case 'ShowHome':
-                 return $this->redirect($this->get('router')->generate('universibo_legacy_home', array(), true), 301);
+                return $this->redirect($this->get('router')->generate('universibo_legacy_home', array(), true), 301);
             case 'ShowInfoDidattica':
                 return $this->redirect($this->get('router')->generate('universibo_legacy_insegnamento_info', array('id_canale' => $_GET['id_canale']), true), 301);
             case 'ShowInsegnamento':
