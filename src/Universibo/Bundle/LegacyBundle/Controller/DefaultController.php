ca5e4dd7c424ebc2f22566035bcb0c4896d694a4
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-01 16:06:37 +0200

    Adding legacy redirects

--- src/Universibo/Bundle/LegacyBundle/Controller/DefaultController.php
+++ src/Universibo/Bundle/LegacyBundle/Controller/DefaultController.php
@@ -47,6 +47,8 @@ class DefaultController extends Controller
                 return $this->redirect($this->get('router')->generate('universibo_legacy_canale', array('id_canale' => $_GET['id_canale']), true), 301);
             case 'ShowCdl':
                 return $this->redirect($this->get('router')->generate('universibo_legacy_cdl', array('id_canale' => $_GET['id_canale']), true), 301);
+            case 'ShowCredits':
+                return $this->redirect($this->get('router')->generate('universibo_legacy_credits', array(), true), 301);
             case 'ShowError':
                 return $this->redirect($this->get('router')->generate('universibo_legacy_error', array(), true), 301);
             case 'ShowFacolta':
@@ -59,6 +61,8 @@ class DefaultController extends Controller
                 return $this->redirect($this->get('router')->generate('universibo_legacy_insegnamento_info', array('id_canale' => $_GET['id_canale']), true), 301);
             case 'ShowInsegnamento':
                 return $this->redirect($this->get('router')->generate('universibo_legacy_insegnamento', array('id_canale' => $_GET['id_canale']), true), 301);
+            case 'ShowManifesto':
+                  return $this->redirect($this->get('router')->generate('universibo_legacy_manifesto', array(), true), 301);
             case 'ShowPermalink':
                 return $this->redirect($this->get('router')->generate('universibo_legacy_permalink', array('id_notizia' => $_GET['id_notizia']), true), 301);
             case 'ShowUser':
