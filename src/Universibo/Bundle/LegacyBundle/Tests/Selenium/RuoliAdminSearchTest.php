fe4cecbee48dade7c8856350f757d9abaf142593
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-01 16:40:27 +0200

    Adding testNotLogged to RuoliAdminSearchTest

--- src/Universibo/Bundle/LegacyBundle/Tests/Selenium/RuoliAdminSearchTest.php
+++ src/Universibo/Bundle/LegacyBundle/Tests/Selenium/RuoliAdminSearchTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Universibo\Bundle\LegacyBundle\Tests\Selenium;
 
+use Universibo\Bundle\LegacyBundle\Tests\TestConstants;
+
 class RuoliAdminSearchTest extends UniversiBOSeleniumTestCase
 {
     protected function setUp()
@@ -8,13 +10,27 @@ class RuoliAdminSearchTest extends UniversiBOSeleniumTestCase
         parent::setUp();
     }
 
+    public function testNotLogged()
+    {
+        $this->logout();
+        $this->openPrefix('/role/admin/search/1415/');
+
+        $this->assertSentences(array(
+                'Error',
+                'Non hai i diritti per modificare i diritti degli utenti su questa pagina. La sessione potrebbe essere scaduta.'
+        ));
+    }
+
     public function testSimple()
     {
+        $this->login(TestConstants::ADMIN_USERNAME);
+
         $this->openPrefix('/role/admin/search/1415/');
         $this->assertSentences(array(
                 'Modifica i diritti nella pagina',
                 'Area collaboratori'
         ));
+
         $this->markTestIncomplete('Just stubbed');
     }
 }
