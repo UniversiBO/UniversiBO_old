040c8d7621fd91d60a02a749c915d6fa09f4516d
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-12 21:09:52 +0200

    Moving PrivacyController back to WebsiteBundle

--- src/Universibo/Bundle/WebsiteBundle/Controller/PrivacyController.php
+++ src/Universibo/Bundle/WebsiteBundle/Controller/PrivacyController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Universibo\Bundle\LegacyBundle\Controller;
+namespace Universibo\Bundle\WebsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
