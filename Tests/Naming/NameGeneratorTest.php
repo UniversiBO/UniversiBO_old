8e25cab5e85b47671fbc45593f19c1e378511972
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-12 23:04:29 +0200

    Refactoring NameGenerator

--- Tests/Naming/NameGeneratorTest.php
+++ Tests/Naming/NameGeneratorTest.php
@@ -11,7 +11,7 @@ class NameGeneratorTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->generator = new NameGenerator(self::YEAR);
+        $this->generator = new NameGenerator();
     }
 
     public function testUpdateOldType()
@@ -19,7 +19,7 @@ class NameGeneratorTest extends \PHPUnit_Framework_TestCase
         $in = 'COSTRUZIONE DI MACCHINE - aa 2003/2004/2005 - Curioni Sergio';
         $expected = 'COSTRUZIONE DI MACCHINE - aa 2003/../2012 - Curioni Sergio';
 
-        $this->assertEquals($expected, $this->generator->update($in));
+        $this->assertEquals($expected, $this->generator->update($in, self::YEAR));
     }
 
     public function testUpdateNewType()
@@ -27,13 +27,13 @@ class NameGeneratorTest extends \PHPUnit_Framework_TestCase
         $in  = 'COSTRUZIONE DI MACCHINE - aa 2003/../2011 - Curioni Sergio';
         $expected = 'COSTRUZIONE DI MACCHINE - aa 2003/../2012 - Curioni Sergio';
 
-        $this->assertEquals($expected, $this->generator->update($in));
+        $this->assertEquals($expected, $this->generator->update($in, self::YEAR));
     }
 
     public function testGenerate()
     {
         $expected = 'COSTRUZIONE DI MACCHINE - aa 2011/2012 - Curioni Sergio';
 
-        $this->assertEquals($expected, $this->generator->generate('COSTRUZIONE DI MACCHINE', 'Curioni Sergio'));
+        $this->assertEquals($expected, $this->generator->generate('COSTRUZIONE DI MACCHINE', 'Curioni Sergio', self::YEAR));
     }
 }
