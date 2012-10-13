13bf3cad2c4d98f09277f3e783c801876dfb2127
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-14 01:54:41 +0200

    Done with common logout close #152

---
+++ src/Universibo/Bundle/ForumBundle/DAO/PhpBB3SessionDAO.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Universibo\Bundle\ForumBundle\DAO;
+
+class PhpBB3SessionDAO extends AbstractDAO implements SessionDAOInterface
+{
+    /**
+     * @param  string $id
+     * @return boolean
+     */
+    public function delete($id)
+    {
+        $conn = $this->getConnection();
+        
+        $sql = <<<EOF
+DELETE 
+    FROM {$this->getPrefix()}sessions
+    WHERE
+        session_id = ?
+EOF;
+
+        return $conn->executeUpdate($sql, array($id));
+    }
+}
