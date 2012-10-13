a3afe178aa83dfc7cb3923efe76921d44f8878b7
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-14 01:06:33 +0200

    phpBB3 LogoutHandler stub #152

--- Security/Http/Logout/PhpBB3LogoutHandler.php
+++ Security/Http/Logout/PhpBB3LogoutHandler.php
@@ -5,11 +5,31 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
+use Universibo\Bundle\ForumBundle\DAO\ConfigDAOInterface;
 
 class PhpBB3LogoutHandler implements LogoutHandlerInterface
+
 {
+    /**
+     * @var ConfigDAOInterface 
+     */
+    private $configDAO;
+    
+    
+    /**
+     * @param \Universibo\Bundle\ForumBundle\DAO\ConfigDAOInterface $configDAO
+     */
+    public function __construct(ConfigDAOInterface $configDAO)
+    {
+        $this->configDAO = $configDAO;
+    }
+    
     public function logout(Request $request, Response $response,
             TokenInterface $token)
     {
+        $cookieDomain = $this->configDAO->getValue('cookie_domain');
+        $cookieName = $this->configDAO->getValue('cookie_name');
+        $cookiePath = $this->configDAO->getValue('cookie_path');
+        $cookieSecure = $this->configDAO->getValue('cookie_secure');
     }
 }
