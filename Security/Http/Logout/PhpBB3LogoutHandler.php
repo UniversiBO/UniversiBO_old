cf552dcb48aa68cc93c489ae90fc280b77e2c58e
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-14 01:46:11 +0200

    LogoutHandler should be ok #152

--- Security/Http/Logout/PhpBB3LogoutHandler.php
+++ Security/Http/Logout/PhpBB3LogoutHandler.php
@@ -1,35 +1,51 @@
 <?php
 namespace Universibo\Bundle\ForumBundle\Security\Http\Logout;
 
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
 use Universibo\Bundle\ForumBundle\DAO\ConfigDAOInterface;
+use Universibo\Bundle\ForumBundle\DAO\SessionDAOInterface;
 
 class PhpBB3LogoutHandler implements LogoutHandlerInterface
 
 {
     /**
-     * @var ConfigDAOInterface 
+     * @var ConfigDAOInterface
      */
     private $configDAO;
     
-    
-    /**
-     * @param \Universibo\Bundle\ForumBundle\DAO\ConfigDAOInterface $configDAO
+    /*
+     * @var SessionDAOInterface
      */
-    public function __construct(ConfigDAOInterface $configDAO)
+    private $sessionDAO;
+
+    /**
+     * @param ConfigDAOInterface $configDAO
+     */
+    public function __construct(ConfigDAOInterface $configDAO,
+            SessionDAOInterface $sessionDAO)
     {
         $this->configDAO = $configDAO;
+        $this->sessionDAO = $sessionDAO;
     }
-    
+
     public function logout(Request $request, Response $response,
             TokenInterface $token)
     {
-        $cookieDomain = $this->configDAO->getValue('cookie_domain');
-        $cookieName = $this->configDAO->getValue('cookie_name');
-        $cookiePath = $this->configDAO->getValue('cookie_path');
-        $cookieSecure = $this->configDAO->getValue('cookie_secure');
+        $domain = $this->configDAO->getValue('cookie_domain');
+        $name = $this->configDAO->getValue('cookie_name');
+        $path = $this->configDAO->getValue('cookie_path');
+        $secure = $this->configDAO->getValue('cookie_secure');
+
+        $sessionId = $request->cookies->get($name.'_sid');
+        $this->sessionDAO->delete($sessionId);
+        
+        foreach (array('u', 'k', 'sid') as $key) {
+            $response->headers->setCookie(new Cookie($name.'_'.$key, null, 0,
+                    $path, $domain, $secure));
+        }
     }
 }
