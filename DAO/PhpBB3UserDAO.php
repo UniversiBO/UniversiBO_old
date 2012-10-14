ed8c75dbd14efe8a8f7dd4db21d6293a0ddf7eef
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-14 13:19:29 +0200

    Adding PhpBB3UserDAO stub implementation

--- DAO/PhpBB3UserDAO.php
+++ DAO/PhpBB3UserDAO.php
@@ -2,6 +2,20 @@
 
 namespace Universibo\Bundle\ForumBundle\DAO;
 
+use Universibo\Bundle\CoreBundle\Entity\User;
+
 class PhpBB3UserDAO extends AbstractDAO implements UserDAOInterface
 {
+
+    public function create(User $user)
+    {
+    }
+
+    public function exists(User $user)
+    {
+    }
+
+    public function update(User $user)
+    {
+    }
 }
