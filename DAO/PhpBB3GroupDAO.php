4ac9bcbe08eb1b8648803e0792fe4d4cca095b3a
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-14 19:43:04 +0200

    Working on #151

--- DAO/PhpBB3GroupDAO.php
+++ DAO/PhpBB3GroupDAO.php
@@ -4,4 +4,12 @@ namespace Universibo\Bundle\ForumBundle\DAO;
 
 class PhpBB3GroupDAO extends AbstractDAO implements GroupDAOInterface
 {
+
+    public function addUserToGroup(\Universibo\Bundle\CoreBundle\Entity\User $user, $groupId)
+    {
+    }
+
+    public function removeUserFromGroup(\Universibo\Bundle\CoreBundle\Entity\User $user, $groupId)
+    {
+    }
 }
