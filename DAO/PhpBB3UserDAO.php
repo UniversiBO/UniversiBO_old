511fbac63cbb4b83847287526a3d829d95c2aaac
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-15 00:04:45 +0200

    Implementing user creation

--- DAO/PhpBB3UserDAO.php
+++ DAO/PhpBB3UserDAO.php
@@ -11,10 +11,35 @@ class PhpBB3UserDAO extends AbstractDAO implements UserDAOInterface
 {
     public function create(User $user)
     {
+        $query = <<<EOT
+INSERT into {$this->getPrefix()}users
+(
+    username,
+    username_clean,
+    user_email,
+    user_regdate
+)
+VALUES
+    (?, ?, ?, ?)
+EOT;
+
+        return $this->getConnection()->executeUpdate($query, array(
+            $user->getUsername(),
+            $user->getUsernameCanonical(),
+            $user->getEmail(),
+            time()
+        ));
     }
 
     public function exists(User $user)
     {
+        $query = <<<EOT
+SELECT COUNT(*)
+    FROM {$this->getPrefix()}users
+    WHERE username = ?
+EOT;
+
+        return $this->getConnection()->fetchColumn($query, array($user->getUsername())) > 0;
     }
 
     public function update(User $user)
