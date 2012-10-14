c572db2e6c3cd9c21d58c76b5631a3d42967a987
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-14 13:14:49 +0200

    Adding methods to UserDAOInterface

--- DAO/UserDAOInterface.php
+++ DAO/UserDAOInterface.php
@@ -1,9 +1,26 @@
 <?php
 namespace Universibo\Bundle\ForumBundle\DAO;
 
+use Universibo\Bundle\CoreBundle\Entity\User;
+
 /**
  * @author Davide Bellettini <davide.bellettini@gmail.com>
  */
 interface UserDAOInterface
 {
+    /**
+     * @param  User    $user
+     * @return boolean
+     */
+    public function exists(User $user);
+
+    /**
+     * @param User $user
+     */
+    public function create(User $user);
+
+    /**
+     * @param User $user
+     */
+    public function update(User $user);
 }
