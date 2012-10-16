21e6c3ff6debee7cc38115dab0844d39b982be76
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-16 23:14:00 +0200

    Wrote listener and templates, almost there #150

---
+++ src/Universibo/Bundle/WebsiteBundle/Listener/AuthenticationFailedListener.php
@@ -0,0 +1,50 @@
+<?php
+namespace Universibo\Bundle\WebsiteBundle\Listener;
+
+use Swift_Mailer;
+use Swift_Message;
+use Symfony\Component\Templating\EngineInterface;
+use Universibo\Bundle\ShibbolethBundle\Security\Authentication\Event\AuthenticationFailedEvent;
+
+class AuthenticationFailedListener
+{
+    /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+
+    /**
+     * @var EngineInterface
+     */
+    private $templateEngine;
+
+    public function __construct(Swift_Mailer $mailer, EngineInterface $templateEngine)
+    {
+        $this->mailer = $mailer;
+        $this->templateEngine = $templateEngine;
+    }
+
+    public function onAuthenticationFailed(AuthenticationFailedEvent $event)
+    {
+        $claims = $event->getClaims();
+
+        $messageDev = Swift_Message::newInstance()
+            ->setSubject('Autenticazione Fallita')
+            ->setFrom('associazione.universibo@unibo.it')
+            ->setTo('dev_universibo@mama.ing.unibo.it')
+            ->setCc('info_universibo@mama.ing.unibo.it')
+            ->setBody($this->templateEngine->render('UniversiboWebsiteBundle:Shibboleth:emailDev.txt.twig', array('claims' => $claims)))
+        ;
+
+        $messageUser = Swift_Message::newInstance()
+            ->setSubject('Attivazione manuale account UniversiBO')
+            ->setFrom('associazione.universibo@unibo.it')
+            //->setTo($claims['eppn'])
+            ->setCc('info_universibo@mama.ing.unibo.it')
+            ->setBody($this->templateEngine->render('UniversiboWebsiteBundle:Shibboleth:emailUser.txt.twig', array('claims' => $claims)))
+        ;
+
+        $this->mailer->send($messageDev);
+        $this->mailer->send($messageUser);
+    }
+}
