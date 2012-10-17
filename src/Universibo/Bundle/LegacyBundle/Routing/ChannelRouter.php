da1696d5466cb00e5e4bd9405463cb41fe34afd9
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-17 09:52:52 +0200

    Reimplementing ChannelRouter

--- src/Universibo/Bundle/LegacyBundle/Routing/ChannelRouter.php
+++ src/Universibo/Bundle/LegacyBundle/Routing/ChannelRouter.php
@@ -1,12 +1,13 @@
 <?php
 namespace Universibo\Bundle\LegacyBundle\Routing;
-use Symfony\Component\Routing\RouterInterface;
 
+use Symfony\Component\Routing\RouterInterface;
 use Universibo\Bundle\LegacyBundle\Entity\Canale;
 
 /**
  * Channel router
  *
+ * @author Davide Bellettini <davide.bellettini@gmail.com>
  */
 class ChannelRouter
 {
@@ -17,24 +18,48 @@ class ChannelRouter
      */
     private $router;
 
+    /**
+     * @var array
+     */
     private $routes;
 
+    /**
+     * @param RouterInterface $router
+     */
     public function __construct(RouterInterface $router)
     {
         $this->router = $router;
-        $this->routes = array('default' => 'channel_show',
-                self::BASE . 'Facolta' => 'faculty_show');
+        $this->routes = array (
+            self::BASE . 'Cdl' => 'universibo_legacy_show_cdl',
+            self::BASE . 'Facolta' => 'universibo_legacy_show_facolta',
+            self::BASE . 'Insegnamento' => 'universibo_legacy_show_insegnamento',
+            self::BASE . 'PrgattivitaDidattica' => 'universibo_legacy_show_insegnamento',
+
+            'default' => 'universibo_legacy_show_canale',
+        );
     }
 
+    /**
+     * Generates url for a Channel
+     *
+     * @param  Canale  $channel
+     * @param  boolean $absolute
+     * @return string
+     */
     public function generate(Canale $channel, $absolute = false)
     {
         $route = $this->getRoute($channel);
 
-        return $this->router
-                ->generate($route, array('id' => $channel->getIdCanale()),
-                        $absolute);
+        return $this
+            ->router
+            ->generate($route, array('id' => $channel->getIdCanale()), $absolute)
+        ;
     }
 
+    /**
+     * @param  Canale $channel
+     * @return string
+     */
     private function getRoute(Canale $channel)
     {
         return array_key_exists($class = get_class($channel), $this->routes) ? $this
