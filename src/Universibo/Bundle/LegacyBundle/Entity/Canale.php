bc0d631d59b4702744d3e53c554314e1537307b2
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-22 23:32:49 +0200

    Fixing uses in Canale

--- src/Universibo/Bundle/LegacyBundle/Entity/Canale.php
+++ src/Universibo/Bundle/LegacyBundle/Entity/Canale.php
@@ -1,12 +1,8 @@
 <?php
 namespace Universibo\Bundle\LegacyBundle\Entity;
 
+use Error;
 use Symfony\Component\Routing\RouterInterface;
-
-use Symfony\Bundle\FrameworkBundle\Routing\Router;
-
-use \DB;
-use \Error;
 use Universibo\Bundle\LegacyBundle\Framework\FrontController;
 
 /**
