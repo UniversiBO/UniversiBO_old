87aa4b881daa6474e6b50de4ca85aa1e1d3d4c15
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-23 14:26:22 +0200

    Fixing RoleAdminEdit and routing close #187

--- src/Universibo/Bundle/LegacyBundle/Command/RuoliAdminEdit.php
+++ src/Universibo/Bundle/LegacyBundle/Command/RuoliAdminEdit.php
@@ -2,6 +2,7 @@
 namespace Universibo\Bundle\LegacyBundle\Command;
 
 use Error;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Universibo\Bundle\CoreBundle\Entity\User;
 use Universibo\Bundle\LegacyBundle\App\UniversiboCommand;
 use Universibo\Bundle\LegacyBundle\Entity\Canale;
@@ -33,25 +34,29 @@ class RuoliAdminEdit extends UniversiboCommand
         $user_ruoli = $user instanceof User ? $ruoloRepo->findByIdUtente($user->getId()) : array();
         $ruoli = array();
         $arrayPublicUsers = array();
+        
+        $request = $this->getRequest();
+        $channelId = $request->get('id_canale');
+        
+        $channelRepo = $this->get('universibo_legacy.repository.canale2');
+        $channel = $channelRepo->find($channelId);
+        
+        if(!$channel instanceof Canale) {
+            throw new NotFoundHttpException('Channel not foun');
+        }
+        
+        $userRepo = $this->get('universibo_core.repository.user');
 
-        if (!array_key_exists('id_canale', $_GET) || !preg_match('/^([0-9]{1,9})$/', $_GET['id_canale']))
-            Error :: throwError(_ERROR_DEFAULT, array ('id_utente' => $user->getId(), 'msg' => 'L\'id del canale richiesto non e` valido', 'file' => __FILE__, 'line' => __LINE__));
-
-        $canale = Canale::retrieveCanale($_GET['id_canale']);
-        $id_canale = $canale->getIdCanale();
-
-        if (!array_key_exists('id_utente', $_GET) || !preg_match('/^([0-9]{1,9})$/', $_GET['id_utente']))
-            Error :: throwError(_ERROR_DEFAULT, array ('id_utente' => $user->getId(), 'msg' => 'L\'id dell\'utente richiesto non e` valido', 'file' => __FILE__, 'line' => __LINE__));
-
-        $target_user = User::selectUser($_GET['id_utente']);
+        $targetUserId = $request->get('id_utente');
+        $target_user = $userRepo->find($targetUserId);
         $target_username = $target_user->getUsername();
         $target_userUri = $router->generate('universibo_legacy_user', array('id_utente' => $target_user->getId()));
 
-        $template->assign('common_canaleURI', $canale->showMe($router));
-        $template->assign('common_langCanaleNome', 'a '.$canale->getTitolo());
+        $template->assign('common_canaleURI', $channel->showMe($router));
+        $template->assign('common_langCanaleNome', 'a '.$channel->getTitolo());
 
-        if (array_key_exists($id_canale, $user_ruoli)) {
-            $ruolo = & $user_ruoli[$id_canale];
+        if (array_key_exists($channelId, $user_ruoli)) {
+            $ruolo = $user_ruoli[$channelId];
             $referente = $ruolo->isReferente();
         }
 
@@ -62,10 +67,10 @@ class RuoliAdminEdit extends UniversiboCommand
             Error :: throwError(_ERROR_DEFAULT, array ('id_utente' => $user->getId(), 'msg' => 'non e` permesso modificare i propri diritti in una pagina', 'file' => __FILE__, 'line' => __LINE__));
 
         $target_ruoli = $ruoloRepo->findByIdUtente($target_user->getId());
-        if (!array_key_exists($id_canale, $target_ruoli))
+        if (!array_key_exists($channelId, $target_ruoli))
             $target_ruolo = null;
         else
-            $target_ruolo = $target_ruoli[$id_canale];
+            $target_ruolo = $target_ruoli[$channelId];
 
         $success = false;
         $f17_accept = false;
@@ -80,7 +85,7 @@ class RuoliAdminEdit extends UniversiboCommand
 
             if ($target_ruolo == null) {
                 $nascosto = false;
-                $ruolo = new Ruolo($target_user->getId(), $id_canale, '' , time(), $_POST['f17_livello'] == 'M', $_POST['f17_livello'] == 'R', true, NOTIFICA_ALL, $nascosto);
+                $ruolo = new Ruolo($target_user->getId(), $channelId, '' , time(), $_POST['f17_livello'] == 'M', $_POST['f17_livello'] == 'R', true, NOTIFICA_ALL, $nascosto);
                 $ruolo->insertRuolo();
 
                 $success = true;
@@ -105,7 +110,7 @@ class RuoliAdminEdit extends UniversiboCommand
         $template->assign('ruoliAdminEdit_username', $target_username);
         $template->assign('ruoliAdminEdit_userUri', $target_userUri);
 
-        $template->assign('ruoliAdminEdit_langAction', "Modifica i diritti nella pagina\n".$canale->getTitolo());
+        $template->assign('ruoliAdminEdit_langAction', "Modifica i diritti nella pagina\n".$channel->getTitolo());
         $template->assign('ruoliAdminEdit_langSuccess', '');
         if ($success == true)
             $template->assign('ruoliAdminEdit_langSuccess', 'Modifica eseguita con successo');
