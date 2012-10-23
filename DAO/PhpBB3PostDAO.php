7ea5b5479cc3316baa82c9f73a060814b9d84496
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-23 11:44:00 +0200

    Implementing MergeableRepositoryInterface

--- DAO/PhpBB3PostDAO.php
+++ DAO/PhpBB3PostDAO.php
@@ -2,11 +2,64 @@
 
 namespace Universibo\Bundle\ForumBundle\DAO;
 
+use Doctrine\DBAL\Connection;
+use Universibo\Bundle\CoreBundle\Entity\User;
+
 /**
  * @author Davide Bellettini <davide.bellettini@gmail.com>
  */
 class PhpBB3PostDAO extends AbstractDAO implements PostDAOInterface
 {
+    /**
+     * @var UserDAOInterface 
+     */
+    private $userDAO;
+    
+    public function __construct(Connection $connection, UserDAOInterface $userDAO)
+    {
+        parent::__construct($connection);
+        
+        $this->userDAO = $userDAO;
+    }
+    
+    public function countByUser(User $user)
+    {
+        $userId = $this->userDAO->findOrCreate($user);
+        
+        $query = <<<EOT
+SELECT COUNT(*)
+    FROM
+        {$this->getPrefix()}posts p
+    WHERE
+        p.poster_id = ?
+EOT;
+        $result = $this
+            ->getConnection()
+            ->executeQuery($query, array($userId))
+        ;
+        
+        return $result->fetchColumn();
+    }
+    
+    public function transferOwnership(User $source, User $target)
+    {
+        $sourceId =  $this->userDAO->findOrCreate($source);
+        $targetId =  $this->userDAO->findOrCreate($target);
+        
+        $query = <<<EOT
+UPDATE {$this->getPrefix()}posts p
+    SET
+        p.poster_id = ?
+    WHERE
+        p.poster_id = ?
+EOT;
+
+        return $this
+            ->getConnection()
+            ->executeUpdate($query, array($targetId, $sourceId))
+        ;
+    }
+    
     public function getLatestPosts($forumId, $limit)
     {
         $query = <<<EOF
