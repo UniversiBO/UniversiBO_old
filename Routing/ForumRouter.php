dc49d00630bee04b69fc83b25083325275f262f2
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-28 02:04:29 +0200

    Adding getPostUri to ForumRouter

--- Routing/ForumRouter.php
+++ Routing/ForumRouter.php
@@ -1,12 +1,19 @@
 <?php
-
+/**
+ * @author Davide Bellettini <davide.bellettini@gmail.com>
+ */
 namespace Universibo\Bundle\ForumBundle\Routing;
 
 use Universibo\Bundle\ForumBundle\Security\ForumSession\ForumSessionInterface;
 
+/**
+ * Forum Router Class
+ */
 class ForumRouter
 {
     /**
+     * Forum session
+     * 
      * @var ForumSessionInterface
      */
     private $forumSession;
@@ -19,12 +26,38 @@ class ForumRouter
         $this->forumSession = $forumSession;
     }
 
+    /**
+     * Gets a forum uri
+     * 
+     * @param integer $forumId
+     * @return string
+     */
     public function getForumUri($forumId)
     {
+        return $this->addSid('/forum/viewforum.php?f='.$forumId);
+    }
+    
+    /**
+     * Gets a post uri
+     * 
+     * @param integer $postId
+     * @return string
+     */
+    public function getPostUri($postId)
+    {
+        return $this->addSid('/forum/viewtopic.php?p='.$postId);
+    }
+    
+    /**
+     * Appends the session ID
+     * 
+     * @param string $uri
+     * @return string
+     */
+    private function addSid($uri)
+    {
         $sid = $this->forumSession->getSessionId();
-
-        $uri = '/forum/viewforum.php?f='.$forumId;
-
+        
         if (strlen($sid) > 0) {
             $uri .= '&sid='.$sid;
         }
