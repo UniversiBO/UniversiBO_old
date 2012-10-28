9ec4a426bda4d79d3be7e1af45d90cda9bd39b9d
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-28 21:23:06 +0100

    Improving cleaning query

--- app/sql/pgsql/query_pulizia_dati_sensibili.sql
+++ app/sql/pgsql/query_pulizia_dati_sensibili.sql
@@ -1,11 +1,11 @@
 UPDATE utente SET password='ac28c2b41697aa58a24449437bade547', salt = '', algoritmo='md5';
-UPDATE utente SET email='prova@example.com';
-UPDATE utente SET ad_username='prova@studio.unibo.it' WHERE ad_username IS NOT NULL OR ad_username != '' ;
+UPDATE utente SET email=user_email='fake.email'||id_utente||'@example.com';
+UPDATE utente SET ad_username='nome.cognome' || id_utente || '@studio.unibo.it' WHERE ad_username IS NOT NULL OR ad_username != '' ;
 UPDATE utente SET phone='0000000000' WHERE phone IS NOT NULL OR phone != '' ;
 
 
 UPDATE phpbb_users SET user_password='ac28c2b41697aa58a24449437bade547';
-UPDATE phpbb_users SET user_email='prova@example.com';
+UPDATE phpbb_users SET user_email='fake.email'||user_id||'@example.com';
 
 
 UPDATE phpbb_posts_text SET post_text='finto testo finto testo finto testo finto testo finto testo finto testo finto testo finto testo finto testo finto testo finto testo finto testo 
