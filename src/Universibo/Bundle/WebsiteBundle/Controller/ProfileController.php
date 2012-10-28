e98bf60fabb18e8b36189bfc29b32468205278d7
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-28 19:34:39 +0100

    Bugfixing ProfileController

--- src/Universibo/Bundle/WebsiteBundle/Controller/ProfileController.php
+++ src/Universibo/Bundle/WebsiteBundle/Controller/ProfileController.php
@@ -3,11 +3,13 @@
 /*
  * Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
+
 namespace Universibo\Bundle\WebsiteBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Universibo\Bundle\WebsiteBundle\Form\UserType;
+
 /**
  */
 class ProfileController extends Controller
@@ -18,9 +20,10 @@ class ProfileController extends Controller
     public function editAction()
     {
         $user = $this->get('security.context')->getToken()->getUser();
+        $this->ensureAtLeastOneContact($user, $this->getDoctrine()->getEntityManager());
 
         $form = $this
-            ->createForm(new UserType(), $user)
+                ->createForm(new UserType(), $user)
         ;
 
         $infoEmail = $this->container->getParameter('mailer_info');
@@ -60,13 +63,7 @@ class ProfileController extends Controller
                 $em->remove($contact);
             }
 
-            if (count($user->getContacts()) === 0) {
-                $contact = new \Universibo\Bundle\CoreBundle\Entity\Contact();
-                $contact->setUser($user);
-                $email = $user->getEmail();
-                $contact->setValue($email);
-                $em->persist($contact);
-            }
+            $this->ensureAtLeastOneContact($user, $em);
 
             $userManager->updateUser($user);
             $em->flush();
@@ -74,7 +71,18 @@ class ProfileController extends Controller
             $this->get('session')->setFlash('notice', 'Il profilo è stato aggiornato');
         }
 
-        return $this->redirect($this->generateUrl('universibo_website_profile_edit',array(), true));
+        return $this->redirect($this->generateUrl('universibo_website_profile_edit', array(), true));
+    }
+
+    private function ensureAtLeastOneContact($user, $em)
+    {
+        if (count($user->getContacts()) === 0) {
+            $contact = new \Universibo\Bundle\CoreBundle\Entity\Contact();
+            $contact->setUser($user);
+            $email = $user->getEmail();
+            $contact->setValue($email);
+            $em->persist($contact);
+        }
     }
 
     private function getUserForm()
@@ -82,7 +90,8 @@ class ProfileController extends Controller
         $user = $this->get('security.context')->getToken()->getUser();
 
         return $this
-            ->createForm(new UserType(), $user)
+                        ->createForm(new UserType(), $user)
         ;
     }
+
 }
