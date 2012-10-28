49b3def0e0b2da0eb7a7831b83a8ed3c2fbdbaf1
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-28 15:15:01 +0100

    Adding Form Type for User

---
+++ Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Universibo\Bundle\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('enabled')
+            ->add('salt')
+            ->add('password')
+            ->add('phone')
+            ->add('notifications')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Universibo\Bundle\CoreBundle\Entity\User'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'universibo_bundle_corebundle_usertype';
+    }
+}
