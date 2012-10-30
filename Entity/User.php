b16ce926ec154e5cb94b645f86730665a9e1a0d4
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-30 23:19:26 +0100

    Adding missing use

--- Entity/User.php
+++ Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace Universibo\Bundle\CoreBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\AdvancedEncoderBundle\Security\Encoder\EncoderAwareInterface;
