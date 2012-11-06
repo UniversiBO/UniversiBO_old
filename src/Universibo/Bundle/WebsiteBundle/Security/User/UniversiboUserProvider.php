b2b328dd967f78c16a7de3b646f2a0bf27a7586c
Davide Bellettini <davide.bellettini@gmail.com>
2012-11-06 19:32:18 +0100

    TDD UniversiboUserProvider, simple #7

--- src/Universibo/Bundle/WebsiteBundle/Security/User/UniversiboUserProvider.php
+++ src/Universibo/Bundle/WebsiteBundle/Security/User/UniversiboUserProvider.php
@@ -2,11 +2,57 @@
 
 namespace Universibo\Bundle\WebsiteBundle\Security\User;
 
+use Symfony\Component\Security\Core\User\User;
+use Universibo\Bundle\CoreBundle\Entity\PersonRepository;
+use Universibo\Bundle\CoreBundle\Entity\UserRepository;
 use Universibo\Bundle\ShibbolethBundle\Security\User\ShibbolethUserProviderInterface;
 
+/**
+ * UniversiBO user provider, converts claims to user
+ *
+ * @author Davide Bellettini <davide.bellettini@gmail.com>
+ */
 class UniversiboUserProvider implements ShibbolethUserProviderInterface
 {
+    /**
+     * Person repository
+     *
+     * @var PersonRepository
+     */
+    private $personRepository;
+
+    /**
+     * User repository
+     *
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * Class constructor
+     *
+     * @param PersonRepository $personRepository
+     * @param UserRepository   $userRepository
+     */
+    public function __construct(PersonRepository $personRepository,
+            UserRepository $userRepository)
+    {
+        $this->personRepository = $personRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Loads the user given claims
+     *
+     * @param  array     $claims
+     * @return User|null
+     */
     public function loadUserByClaims(array $claims)
     {
+        $uniboId = $claims['idAnagraficaUnica'];
+
+        $person = $this->personRepository->findOneByUniboId($uniboId);
+
+        return $this->userRepository->findOneNotLocked($person);
     }
 }
--- src/Universibo/Bundle/WebsiteBundle/Tests/Security/User/UniversiboUserProviderTest.php
+++ src/Universibo/Bundle/WebsiteBundle/Tests/Security/User/UniversiboUserProviderTest.php
@@ -2,6 +2,10 @@
 
 namespace Universibo\Bundle\WebsiteBundle\Tests\Security\User;
 
+use Universibo\Bundle\CoreBundle\Entity\Person;
+use Universibo\Bundle\CoreBundle\Entity\PersonRepository;
+use Universibo\Bundle\CoreBundle\Entity\User;
+use Universibo\Bundle\CoreBundle\Entity\UserRepository;
 use Universibo\Bundle\WebsiteBundle\Security\User\UniversiboUserProvider;
 
 class UniversiboUserProviderTest extends \PHPUnit_Framework_TestCase
@@ -29,25 +33,45 @@ class UniversiboUserProviderTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->personRepository = $this->getMock('Universibo\\Bundle\\CoreBundle\\Entity\\PersonRepository', array(), array(), '', false);
+        $this->personRepository = $this->getMock('Universibo\\Bundle\\CoreBundle\\Entity\\PersonRepository', array('findOneByUniboId'), array(), '', false);
         $this->userRepository = $this->getMock('Universibo\\Bundle\\CoreBundle\\Entity\\UserRepository', array(), array(), '', false);
         $this->provider = new UniversiboUserProvider($this->personRepository, $this->userRepository);
     }
 
     public function testExistingPersonSimple()
     {
+        $person = new Person();
+        $person->setUniboId(42);
+        $person->setGivenName('Nome');
+        $person->setSurname('cognome');
+
+        $mockedUser = new User();
+        $mockedUser->setPerson($person);
+        $mockedUser->setEmail('nome.cognome@unibo.it');
+
         $claims = array (
-            'eppn' => 'nome.cognome@unibo.it',
-            'idAnagraficaUnica' => 42,
+            'eppn' => $mockedUser->getEmail(),
+            'idAnagraficaUnica' => $person->getUniboId(),
             'isMemberOf' => 'Docente',
-            'nome' => 'Nome',
-            'cognome' => 'Cognome',
+            'givenName' => $person->getGivenName(),
+            'sn' => $person->getSurname()
         );
+
+        $this->personRepository
+             ->expects($this->atLeastOnce())
+             ->method('findOneByUniboId')
+             ->with($this->equalTo($person->getUniboId()))
+             ->will($this->returnValue($person));
+
+        $this->userRepository
+             ->expects($this->atLeastOnce())
+             ->method('findOneNotLocked')
+             ->with($this->equalTo($person))
+             ->will($this->returnValue($mockedUser));
 
         $user = $this->provider->loadUserByClaims($claims);
 
         $this->assertInstanceOf('Universibo\\Bundle\\CoreBundle\\Entity\\User', $user);
-
-        $this->markTestIncomplete();
+        $this->assertEquals($mockedUser, $user);
     }
 }
