05bf1d852b32311decdabba854ca44eb98b24b31
Davide Bellettini <davide.bellettini@gmail.com>
2012-11-07 12:17:03 +0100

    TDD new test cases for UniversiboUserProvider #7

--- src/Universibo/Bundle/WebsiteBundle/Security/User/UniversiboUserProvider.php
+++ src/Universibo/Bundle/WebsiteBundle/Security/User/UniversiboUserProvider.php
@@ -3,10 +3,13 @@
 namespace Universibo\Bundle\WebsiteBundle\Security\User;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\User\User;
+use Doctrine\ORM\NoResultException;
+use FOS\UserBundle\Model\UserManagerInterface;
 use Universibo\Bundle\CoreBundle\Entity\Person;
 use Universibo\Bundle\CoreBundle\Entity\PersonRepository;
+use Universibo\Bundle\CoreBundle\Entity\User;
 use Universibo\Bundle\CoreBundle\Entity\UserRepository;
+use Universibo\Bundle\LegacyBundle\Auth\LegacyRoles;
 use Universibo\Bundle\ShibbolethBundle\Security\User\ShibbolethUserProviderInterface;
 
 /**
@@ -38,19 +41,48 @@ class UniversiboUserProvider implements ShibbolethUserProviderInterface
     private $userRepository;
 
     /**
+     * User manager
+     *
+     * @var UserManagerInterface
+     */
+    private $userManager;
+
+    /**
+     * MemberOf handlers
+     *
+     * @var array
+     */
+    private $memberOfHandlers = array();
+
+    /**
      * Class constructor
      *
-     * @param ObjectManager    $objectManager
-     * @param PersonRepository $personRepository
-     * @param UserRepository   $userRepository
+     * @param ObjectManager        $objectManager
+     * @param PersonRepository     $personRepository
+     * @param UserRepository       $userRepository
+     * @param UserManagerInterface $userManager
      */
     public function __construct(ObjectManager $objectManager,
-            PersonRepository $personRepository,
-            UserRepository $userRepository)
+            PersonRepository $personRepository, UserRepository $userRepository,
+            UserManagerInterface $userManager)
     {
         $this->objectManager = $objectManager;
         $this->personRepository = $personRepository;
         $this->userRepository = $userRepository;
+        $this->userManager = $userManager;
+
+        $this->memberOfHandlers['Docente'] = function($user) {
+            $user->setLegacyGroups(LegacyRoles::DOCENTE);
+        };
+
+        $this->memberOfHandlers['Studente'] = function($user) {
+            $user->setLegacyGroups(LegacyRoles::STUDENTE);
+            $user->setUsernameLocked(false);
+        };
+
+        $this->memberOfHandlers['default'] = function($user) {
+            $user->setLegacyGroups(LegacyRoles::PERSONALE);
+        };
     }
 
     /**
@@ -62,12 +94,40 @@ class UniversiboUserProvider implements ShibbolethUserProviderInterface
     public function loadUserByClaims(array $claims)
     {
         $uniboId = $claims['idAnagraficaUnica'];
+        $eppn = $claims['eppn'];
 
         $person = $this->findOrCreatePerson($uniboId, $claims['givenName'], $claims['sn']);
 
-        return $this->userRepository->findOneNotLocked($person);
+        $user = $this->findUserByPerson($person);
+        if ($user === null) {
+            $user = $this->userManager->findUserByEmail($eppn);
+        }
+
+        if ($user === null) {
+            $user = new User();
+            $user->setUsername($this->getAvailableUsername($eppn));
+            $user->setEmail($eppn);
+
+            $memberOf = $claims['isMemberOf'];
+
+            $key = array_key_exists($memberOf, $this->memberOfHandlers) ? $memberOf : 'default';
+            $this->memberOfHandlers[$key]($user);
+        }
+
+        $user->setPerson($person);
+        $this->userManager->updateUser($user);
+
+        return $user;
     }
 
+    /**
+     * Finds a person or creates it
+     *
+     * @param  int    $uniboId
+     * @param  string $givenName
+     * @param  string $surname
+     * @return Person
+     */
     private function findOrCreatePerson($uniboId, $givenName, $surname)
     {
         $person = $this->personRepository->findOneByUniboId($uniboId);
@@ -81,5 +141,35 @@ class UniversiboUserProvider implements ShibbolethUserProviderInterface
         $person->setSurname($surname);
 
         return $this->objectManager->merge($person);
+    }
+
+    /**
+     * Finds a user by person
+     *
+     * @param  Person    $person
+     * @return User|null
+     */
+    private function findUserByPerson(Person $person)
+    {
+        try {
+            return $this->userRepository->findOneNotLocked($person);
+        } catch (NoResultException $e) {
+            return null;
+        }
+    }
+
+    private function getAvailableUsername($eppn)
+    {
+        list($username, $domain) = split('@', $eppn);
+
+        // TODO fake implementation
+        $targetUsername = $username;
+        $i = 1;
+
+        while ($this->userManager->findUserByUsername($targetUsername) !== null) {
+            $targetUsername = $username . $i++;
+        }
+
+        return $targetUsername;
     }
 }
--- src/Universibo/Bundle/WebsiteBundle/Tests/Security/User/UniversiboUserProviderTest.php
+++ src/Universibo/Bundle/WebsiteBundle/Tests/Security/User/UniversiboUserProviderTest.php
@@ -3,6 +3,8 @@
 namespace Universibo\Bundle\WebsiteBundle\Tests\Security\User;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\NoResultException;
+use FOS\UserBundle\Model\UserManagerInterface;
 use Universibo\Bundle\CoreBundle\Entity\Person;
 use Universibo\Bundle\CoreBundle\Entity\PersonRepository;
 use Universibo\Bundle\CoreBundle\Entity\User;
@@ -40,13 +42,22 @@ class UniversiboUserProviderTest extends \PHPUnit_Framework_TestCase
      */
     private $userRepository;
 
+    /**
+     * User manager
+     *
+     * @var UserManagerInterface
+     */
+    private $userManager;
+
     protected function setUp()
     {
         $this->objectManager = $this->getMock('Doctrine\\Common\\Persistence\\ObjectManager');
         $this->personRepository = $this->getMock('Universibo\\Bundle\\CoreBundle\\Entity\\PersonRepository', array('findOneByUniboId'), array(), '', false);
         $this->userRepository = $this->getMock('Universibo\\Bundle\\CoreBundle\\Entity\\UserRepository', array('findOneByEmail', 'findOneNotLocked'), array(), '', false);
+        $this->userManager = $this->getMock('FOS\\UserBundle\\Model\\UserManagerInterface');
+
         $this->provider = new UniversiboUserProvider($this->objectManager,
-                $this->personRepository, $this->userRepository);
+                $this->personRepository, $this->userRepository, $this->userManager);
     }
 
     public function testExistingPersonNoUserSimple()
@@ -80,10 +91,19 @@ class UniversiboUserProviderTest extends \PHPUnit_Framework_TestCase
              ->with($this->equalTo($person))
              ->will($this->returnValue($mockedUser));
 
+        $this
+            ->objectManager
+            ->expects($this->atLeastOnce())
+            ->method('merge')
+            ->will($this->returnArgument(0))
+        ;
+
         $user = $this->provider->loadUserByClaims($claims);
 
         $this->assertInstanceOf('Universibo\\Bundle\\CoreBundle\\Entity\\User', $user);
         $this->assertEquals($mockedUser, $user);
+
+        $this->personAssertions($user->getPerson(), $claims);
     }
 
     /**
@@ -111,11 +131,85 @@ class UniversiboUserProviderTest extends \PHPUnit_Framework_TestCase
         ;
 
         $this
-            ->userRepository
+            ->userManager
             ->expects($this->atLeastOnce())
-            ->method('findOneByEmail')
+            ->method('findUserByEmail')
             ->with($this->equalTo($claims['eppn']))
             ->will($this->returnValue(null))
+        ;
+
+        $this
+            ->userRepository
+            ->expects($this->atLeastOnce())
+            ->method('findOneNotLocked')
+            ->will($this->throwException(new NoResultException()))
+        ;
+
+        $this
+            ->objectManager
+            ->expects($this->atLeastOnce())
+            ->method('merge')
+            ->will($this->returnArgument(0))
+        ;
+
+        $this
+            ->userManager
+            ->expects($this->atLeastOnce())
+            ->method('findUserByUsername')
+            ->will($this->returnValue(null))
+        ;
+
+        $this
+            ->userManager
+            ->expects($this->atLeastOnce())
+            ->method('updateUser')
+        ;
+
+        $user = $this->provider->loadUserByClaims($claims);
+
+        $this->assertInstanceOf('Universibo\\Bundle\\CoreBundle\\Entity\\User', $user);
+        $this->assertEquals($claims['eppn'], $user->getEmail());
+        $this->assertEquals($legacyGroups, $user->getLegacyGroups());
+        $this->assertEquals($usernameLocked, $user->isUsernameLocked());
+
+        $person = $user->getPerson();
+        $this->personAssertions($person, $claims);
+    }
+
+    public function testNoPersonExistingEmailSimple()
+    {
+        $claims = array (
+            'eppn' => 'nome.cognome@unibo.it',
+            'idAnagraficaUnica' => 42,
+            'isMemberOf' => 'Docente',
+            'givenName' => 'Nome',
+            'sn' => 'Cognome'
+        );
+
+        $mockedUser = new User();
+        $mockedUser->setEmail($claims['eppn']);
+        $mockedUser->setUsername('username');
+
+        $this
+            ->personRepository
+            ->expects($this->atLeastOnce())
+            ->method('findOneByUniboId')
+            ->with($this->equalTo($claims['idAnagraficaUnica']))
+            ->will($this->returnValue(null))
+        ;
+
+        $this
+            ->userManager
+            ->expects($this->atLeastOnce())
+            ->method('findUserByEmail')
+            ->with($this->equalTo($claims['eppn']))
+            ->will($this->returnValue($mockedUser))
+        ;
+
+        $this
+            ->userManager
+            ->expects($this->atLeastOnce())
+            ->method('updateUser')
         ;
 
         $this
@@ -127,24 +221,11 @@ class UniversiboUserProviderTest extends \PHPUnit_Framework_TestCase
 
         $user = $this->provider->loadUserByClaims($claims);
 
-        $this->assertInstanceOf('Universibo\\Bundle\\CoreBundle\\Entity\\User', $user);
-        $this->assertEquals($claims['eppn'], $user->getEmail());
-        $this->assertEquals($legacyGroups, $user->getLegacyGroups());
-        $this->assertEquals($usernameLocked, $user->isUsernameLocked());
+        $this->assertSame($mockedUser, $user);
 
         $person = $user->getPerson();
 
-        $this->assertInstanceOf('Universibo\\Bundle\\CoreBundle\\Entity\\Person', $person);
-        $this->assertEquals($claims['idAnagraficaUnica'], $person->getUniboId());
-        $this->assertEquals($claims['givenName'], $person->getGivenName());
-        $this->assertEquals($claims['sn'], $person->getSurname());
-
-        $this->markTestIncomplete();
-    }
-
-    public function testNoPersonExistingEmailSimple()
-    {
-        $this->markTestIncomplete();
+        $this->personAssertions($person, $claims);
     }
 
     public function testExistingPersonDifferentEmailSimple()
@@ -160,11 +241,19 @@ class UniversiboUserProviderTest extends \PHPUnit_Framework_TestCase
     public function provider()
     {
         return array (
-            array('Docente', LegacyRoles::DOCENTE, false),
-            array('Studente', LegacyRoles::STUDENTE, true),
-            array('PersonaleTA', LegacyRoles::PERSONALE, false),
-            array('Esterno', LegacyRoles::PERSONALE, false),
-            array('Accreditato', LegacyRoles::PERSONALE, false),
+            array('Docente', LegacyRoles::DOCENTE, true),
+            array('Studente', LegacyRoles::STUDENTE, false),
+            array('PersonaleTA', LegacyRoles::PERSONALE, true),
+            array('Esterno', LegacyRoles::PERSONALE, true),
+            array('Accreditato', LegacyRoles::PERSONALE, true),
         );
+    }
+
+    private function personAssertions(Person $person, array $claims)
+    {
+        $this->assertInstanceOf('Universibo\\Bundle\\CoreBundle\\Entity\\Person', $person);
+        $this->assertEquals($claims['idAnagraficaUnica'], $person->getUniboId());
+        $this->assertEquals($claims['givenName'], $person->getGivenName());
+        $this->assertEquals($claims['sn'], $person->getSurname());
     }
 }
