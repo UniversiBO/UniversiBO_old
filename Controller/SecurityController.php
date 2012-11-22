8732b5c12ab2cf61048cfc6daff574287b02024a
Davide Bellettini <davide.bellettini@gmail.com>
2012-11-22 14:36:05 +0100

    SecurityController

---
+++ Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Universibo\Bundle\ForumBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Forum security controller
+ */
+class SecurityController extends Controller
+{
+    /**
+     * This action ensures that the user is logged in the forum
+     */
+    public function loginAction(Request $request)
+    {
+        $forumSession = $this->get('universibo_forum.session');
+        $target = $request->query->get('wreply', $this->generateUrl('homepage'));
+        $response = $this->redirect($target);
+         
+        $forumSession->login($this->getUser(), $request, $response);
+        
+        return $response;
+    }
+}
