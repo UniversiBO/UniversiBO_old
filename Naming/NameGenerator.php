c64337ca4f9b754106b500bce361fc40139f8753
Davide Bellettini <davide.bellettini@gmail.com>
2012-12-30 19:17:12 +0100

    Name generator refactoring & comments

--- Naming/NameGenerator.php
+++ Naming/NameGenerator.php
@@ -1,5 +1,10 @@
 <?php
+/**
+ * @license GPLv3
+ * @copyright (c) 2012, Associazione UniversiBO
+ */
 namespace Universibo\Bundle\ForumBundle\Naming;
+
 /**
  * Forum Name Generator
  *
@@ -9,26 +14,41 @@ class NameGenerator
 {
     const SEPARATOR = ' - ';
 
+    /**
+     * Updates the forum name with new year
+     *
+     * @param  string  $title current title
+     * @param  integer $year
+     * @return string  new title
+     */
     public function update($title, $year)
     {
         return preg_replace('/aa ([0-9]{4})(\\/\\.\\.)?(\\/[0-9]{4})+/',
                 'aa $1/../' . $this->getLast($year), $title);
     }
 
+    /**
+     * Generates a new forum name
+     * @param  string  $subjectName
+     * @param  string  $professorName
+     * @param  integer $year
+     * @return string
+     */
     public function generate($subjectName, $professorName, $year)
     {
         return $subjectName . self::SEPARATOR
-                . $this->getAcademicalYearString($year) . self::SEPARATOR
+                . $this->getAcademicYearString($year) . self::SEPARATOR
                 . $professorName;
     }
 
-    protected function getLast($year)
+    /**
+     * Gets Academic year string
+     *
+     * @param  integer $year
+     * @return string
+     */
+    protected function getAcademicYearString($year)
     {
-        return $year + 1;
-    }
-
-    protected function getAcademicalYearString($year)
-    {
-        return 'aa ' . $year . '/' . $this->getLast($year);
+        return 'aa ' . $year . '/' . ($year + 1);
     }
 }
