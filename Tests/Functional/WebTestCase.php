23204e166518e393a2e1500f3ba62c6163b00869
Davide Bellettini <davide.bellettini@gmail.com>
2013-01-13 18:53:05 +0100

    Functional test stub

---
+++ Tests/Functional/WebTestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
+
+/**
+ * Base Functional test case. Inspired (copied) from FOSCommentBundle
+ * functional test suites. 
+ */
+class WebTestCase extends BaseWebTestCase
+{
+    /**
+     * gets the kernel class
+     * 
+     * @return string
+     */
+    protected static function getKernelClass()
+    {
+        require_once __DIR__.'/app/AppKernel.php';
+
+        return 'Universibo\\Bundle\\ForumBundle\\Tests\\Functional\\AppKernel';
+    }
+}
---
+++ Tests/Functional/app/AppKernel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Davide Bellettini
+ * @license GPLv2
+ */
+namespace UniversiboForumBundle\Tests\Functional;
+
+use Symfony\Component\HttpKernel\Kernel;
+
+class AppKernel extends Kernel
+{
+}
