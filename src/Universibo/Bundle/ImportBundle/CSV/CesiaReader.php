27bca14d853ab731df40cbde7cebffc01fb770a5
Davide Bellettini <davide.bellettini@gmail.com>
2013-01-13 18:41:45 +0100

    Overriding delimiter in a cleaner way

--- src/Universibo/Bundle/ImportBundle/CSV/CesiaReader.php
+++ src/Universibo/Bundle/ImportBundle/CSV/CesiaReader.php
@@ -9,15 +9,8 @@ use EasyCSV\Reader;
 class CesiaReader extends Reader
 {
     /**
-     * Class constructor
-     *
-     * @param string $path file name
-     * @param string $mode fopen mode
+     * Cesia-specific delimiter
+     * @var string
      */
-    public function __construct($path, $mode = 'r+')
-    {
-        $this->_delimiter = ';';
-
-        parent::__construct($path, $mode);
-    }
+    protected $delimiter = ';';
 }
