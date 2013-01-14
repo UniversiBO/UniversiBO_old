d0aae3756ce8083291c5c892bbf6883256e5b848
Davide Bellettini <davide.bellettini@gmail.com>
2013-01-14 17:41:47 +0100

    Removing crappy sort implemenation #5

--- Entity/ForumRepository.php
+++ Entity/ForumRepository.php
@@ -28,6 +28,77 @@ class ForumRepository extends Repository
     }
 
     /**
+     * Gets the max forum id
+     *
+     * @return integer|null
+     */
+    public function getMaxId()
+    {
+        $query = "SELECT MAX(forum_id) FROM {$this->getPrefix()}forums";
+
+        return $this
+            ->getConnection()
+            ->fetchColumn($query)
+            ?: null
+        ;
+    }
+
+    /**
+     * Finds a forum by id
+     *
+     * @param  integer    $id
+     * @return Forum|null
+     */
+    public function find($id)
+    {
+       $query = 'SELECT * FROM ' . $this->getPrefix().'forums WHERE forum_id = ?';
+
+       $row = $this
+           ->getConnection()
+           ->fetchAssoc($query, array($id))
+       ;
+
+       return $this->rowToForum($row);
+    }
+
+    /**
+     * Finds a forum by name
+     *
+     * @param  string     $name
+     * @return Forum|null
+     */
+    public function findOneByName($name)
+    {
+        $query = 'SELECT * FROM ' . $this->getPrefix().'forums WHERE forum_name = ?';
+
+        $row = $this
+            ->getConnection()
+            ->fetchAssoc($query, array($name))
+        ;
+
+        return $this->rowToForum($row);
+    }
+
+    public function delete(Forum $forum)
+    {
+        $leftRight = $this->getLeftRight($id = $forum->getId());
+
+        if ($leftRight['left_id'] !== $leftRight['right_id'] - 1) {
+            throw new \LogicException('Delete nested forums first!');
+        }
+
+        $this->incrementLeft($leftRight['left_id'], -2);
+        $this->incrementRight($leftRight['right_id'], -2);
+
+        $query = 'DELETE FROM '.$this->getPrefix().'forums WHERE forum_id = ?';
+
+        return $this
+            ->getConnection()
+            ->executeUpdate($query, array($id)) > 0
+        ;
+    }
+
+     /**
      * Creates a new forum
      *
      * @param Forum $forum
@@ -116,58 +187,6 @@ EOT;
     }
 
     /**
-     * Gets the max forum id
-     *
-     * @return integer|null
-     */
-    public function getMaxId()
-    {
-        $query = "SELECT MAX(forum_id) FROM {$this->getPrefix()}forums";
-
-        return $this
-            ->getConnection()
-            ->fetchColumn($query)
-            ?: null
-        ;
-    }
-
-    /**
-     * Finds a forum by id
-     *
-     * @param  integer    $id
-     * @return Forum|null
-     */
-    public function find($id)
-    {
-       $query = 'SELECT * FROM ' . $this->getPrefix().'forums WHERE forum_id = ?';
-
-       $row = $this
-           ->getConnection()
-           ->fetchAssoc($query, array($id))
-       ;
-
-       return $this->rowToForum($row);
-    }
-
-    /**
-     * Finds a forum by name
-     *
-     * @param  string     $name
-     * @return Forum|null
-     */
-    public function findOneByName($name)
-    {
-        $query = 'SELECT * FROM ' . $this->getPrefix().'forums WHERE forum_name = ?';
-
-        $row = $this
-            ->getConnection()
-            ->fetchAssoc($query, array($name))
-        ;
-
-        return $this->rowToForum($row);
-    }
-
-    /**
      * Gets forum's left_id and right_id
      *
      * @param  integer $id
@@ -241,144 +260,5 @@ EOT;
             ->setType($row['forum_type'])
             ->setParentId($row['parent_id'])
         ;
-    }
-
-    public function delete(Forum $forum)
-    {
-        $leftRight = $this->getLeftRight($id = $forum->getId());
-
-        if ($leftRight['left_id'] !== $leftRight['right_id'] - 1) {
-            throw new \LogicException('Delete nested forums first!');
-        }
-
-        $this->incrementLeft($leftRight['left_id'], -2);
-        $this->incrementRight($leftRight['right_id'], -2);
-
-        $query = 'DELETE FROM '.$this->getPrefix().'forums WHERE forum_id = ?';
-
-        return $this
-            ->getConnection()
-            ->executeUpdate($query, array($id)) > 0
-        ;
-    }
-
-    public function findByParentId($parentId)
-    {
-        $forums = array();
-
-        foreach ($this->findByParentIdArray($parentId) as $row) {
-            $forums[] = $this->rowToForum($row);
-        }
-
-        return $forums;
-    }
-
-    private function findByParentIdArray($parentId)
-    {
-        $sql = <<<EOT
-SELECT *
-    FROM {$this->getPrefix()}forums
-    WHERE
-        parent_id = ?
-    ORDER BY
-        left_id
-EOT;
-
-        return $this
-            ->getConnection()
-            ->fetchAll($sql, array($parentId))
-        ;
-    }
-
-    public function sortAlphabetically($parentId)
-    {
-        $array = $this->findByParentIdArray($parentId);
-
-        $comparingFunction = function($a, $b) {
-            return strcasecmp($a['forum_name'], $b['forum_name']);
-        };
-
-        $changed = array();
-
-        $swappingFunction = function(&$a, &$b) use (&$changed) {
-            $c = $a;
-            $a = $b;
-            $b = $c;
-
-            $this->swapForums($a, $b, $changed);
-        };
-
-        $this->bubbleSort($array, $comparingFunction, $swappingFunction);
-
-        foreach ($changed as $forum) {
-            $this->setLeftRight($forum['forum_id'], $forum);
-        }
-    }
-
-    private function swapForums(&$forum1, &$forum2, array &$changed)
-    {
-        if ($forum1['parent_id'] !== $forum2['parent_id']) {
-            throw new \LogicException('Parent forum not matching');
-        }
-
-        $left1 = $forum1['left_id'];
-        $right1 = $forum1['right_id'];
-
-        $forum1['left_id'] = $forum2['left_id'];
-        $forum1['right_id'] = $forum2['right_id'];
-
-        $forum2['left_id'] = $left1;
-        $forum2['right_id'] = $right1;
-
-        $changed[$forum1['forum_id']] = $forum1;
-        $changed[$forum2['forum_id']] = $forum2;
-    }
-
-    /**
-     * Sets left and right
-     *
-     * @param  Forum|integer $forum
-     * @param  array         $leftRight
-     * @return boolean
-     */
-    private function setLeftRight($forumId, array $leftRight)
-    {
-        if ($forumId instanceof Forum) {
-            $forumId = $forumId->getId();
-        }
-
-        $query = <<<EOT
-UPDATE {$this->getPrefix()}forums
-    SET
-        left_id = ?,
-        right_id = ?
-    WHERE
-        forum_id = ?
-EOT;
-
-        return $this
-            ->getConnection()
-            ->executeUpdate($query, array(
-                $leftRight['left_id'],
-                $leftRight['right_id'],
-                $forumId
-            )) > 0
-        ;
-    }
-
-    private function bubbleSort(array $array, $comparingFunction, $swappingFunction)
-    {
-        $n = count($array);
-
-        do {
-            $swapped = false;
-
-            for ($i=1; $i<$n; $i++) {
-                if ($comparingFunction($array[$i-1], $array[$i]) > 0) {
-                    $swappingFunction($array[$i-1], $array[$i]);
-                    $swapped = true;
-                }
-            }
-        } while ($swapped);
     }
 }
