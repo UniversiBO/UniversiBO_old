d0b049339a4cf272d858acae39a0a88ba1f8be33
Davide Bellettini <davide.bellettini@gmail.com>
2013-01-29 22:25:06 +0100

    Fixing puppet provisoning

--- vagrant/bash/init.sh
+++ vagrant/bash/init.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-aptitude -q update
-gem update
 
-PUPPET_DIR=/etc/puppet/
-
-if [ ! -d $PUPPET_DIR ]; then
-    mkdir $PUPPET_DIR
-fi
 
 if [ `which git | wc -l` -eq 0 ]; then
     aptitude install git
 fi
 
-cp /vagrant/puppet/Puppetfile $PUPPET_DIR
+function puppet_install {
+    clean=`echo "$1" | sed 's/\\//-/'`
+    if [ `puppet module list | grep "$clean" | wc -l` -eq 0 ]; then
+        puppet module install $1
+    fi
+}
 
-if [ `gem query --local | grep librarian-puppet | wc -l` -eq 0 ]; then
-  gem install librarian-puppet
-  cd $PUPPET_DIR && librarian-puppet install --clean
-else
-  cd $PUPPET_DIR && librarian-puppet update
-fi
+puppet_install 'puppetlabs/postgresql'
