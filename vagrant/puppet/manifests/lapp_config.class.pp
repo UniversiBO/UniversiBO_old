02cfe81e26f0d6f95536ffc5a910fbcb2385cfa0
Davide Bellettini <davide.bellettini@gmail.com>
2013-01-29 23:52:33 +0100

    Working on Puppet

--- vagrant/puppet/manifests/lapp_config.class.pp
+++ vagrant/puppet/manifests/lapp_config.class.pp
@@ -1,3 +1,9 @@
+#include postgresql::server
+
 class lapp_config
 {
+#    postgresql::db { 'universibo':
+#        user     => 'universibo',
+#        password => 'universibo'
+#    }
 }
--- vagrant/puppet/manifests/universibo12.pp
+++ vagrant/puppet/manifests/universibo12.pp
@@ -1,3 +1,5 @@
+$postgres_default_version = '9.1'
+
 import 'lapp_packages.class.pp'
 import 'lapp_config.class.pp'
 
@@ -9,7 +11,7 @@ File { owner => 0, group => 0, mode => 0644 }
 
 file { '/etc/motd':
   content => "Welcome to UniversiBO Development Kit
-          Managed by Vagrant & Puppet.\n"
+         Managed by Vagrant & Puppet.\n"
 }
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/sbin", "/usr/local/bin", "/opt/vagrant_ruby/bin" ] }
