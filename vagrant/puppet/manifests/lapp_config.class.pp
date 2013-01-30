dbb49e46524061cf23154db8f648f1ce3892b310
Davide Bellettini <davide.bellettini@gmail.com>
2013-01-30 06:54:20 +0100

    Fixing NameVirtualHost issue

--- vagrant/puppet/manifests/lapp_config.class.pp
+++ vagrant/puppet/manifests/lapp_config.class.pp
@@ -15,13 +15,17 @@ class lapp_config
         command => "sed 's/.*allow from 127.*/Allow from All/i' -i /etc/apache2/conf.d/phppgadmin"
     }
 
+    exec { 'ports.conf':
+        command => "sed '/^NameVirtualHost/d' /etc/apache2/ports.conf"
+    }
+
     exec { 'enable-modules':
         command => 'a2enmod rewrite rpaf' 
     }
 
     exec { 'reload':
         command => 'apache2ctl graceful',
-        require => Exec['allow-all', 'enable-modules']
+        require => Exec['allow-all', 'ports.conf', 'enable-modules']
     }
 
     exec { 'init-db': 
@@ -30,7 +34,7 @@ class lapp_config
 
     apache::vhost { 'default':
         priority        => '10',
-        vhost_name      => '192.168.33.10',
+        vhost_name      => '*',
         port            => '80',
         docroot         => '/vagrant/web',
         logroot         => '/vagrant/app/logs',
