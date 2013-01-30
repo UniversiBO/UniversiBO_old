a31287e8fd0defde7c3c03d735ea5a5f20d4b431
Davide Bellettini <davide.bellettini@gmail.com>
2013-01-30 22:06:54 +0100

    Removing PPA

--- vagrant/scripts/init.sh
+++ vagrant/scripts/init.sh
@@ -4,12 +4,6 @@ PUPPET_MODULES=/etc/puppet/modules
 
 sed 's/us\.archive/it\.archive/' -i /etc/apt/sources.list
 
-if [ `aptitude search $pkg | grep ^i | wc -l` -eq 0 ]; then
-    aptitude update
-    aptitude install python-software-properties --assume-yes
-fi
-
-add-apt-repository ppa:ondrej/php5
 aptitude update
 
 if [ `which git | wc -l` -eq 0 ]; then
