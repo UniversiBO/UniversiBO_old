e09f7bced4f304a1253540ae203059c7896339cf
Davide Bellettini <davide.bellettini@gmail.com>
2013-01-30 10:58:27 +0100

    Done with DB autoloading

--- vagrant/puppet/manifests/universibo_init.class.pp
+++ vagrant/puppet/manifests/universibo_init.class.pp
@@ -1,7 +1,15 @@
 class universibo_init
 {
+    exec {'load-db':
+        cwd => '/vagrant/app/sql/pgsql',
+        command => "cat devdb.sql | sed 's/OWNER TO .*/OWNER TO universibo;/' | su - postgres -c 'psql universibo'",
+        unless => "test `/vagrant/vagrant/scripts/check.tables.php` -gt 0",
+        logoutput => true
+    }
+    
     exec {'load-forum':
-        command => "cat /vagrant/vendor/universibo/forum-bundle/Universibo/Bundle/ForumBundle/Tests/Resources/sql/{structure,data}-postgres.sql | sed 's/OWNER TO .*/OWNER TO universibo;/' | su - postgres -c 'psql universibo_forum3'",
+        cwd => '/vagrant/vendor/universibo/forum-bundle/Universibo/Bundle/ForumBundle/Tests/Resources/sql/',
+        command => "cat structure-postgres.sql data-postgres.sql | sed 's/OWNER TO .*/OWNER TO universibo;/' | su - postgres -c 'psql universibo_forum3'",
         unless => "test `/vagrant/vagrant/scripts/check.forumtables.php` -gt 0"
     }
 }
