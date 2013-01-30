2f4f6416096d822ed04ba5b3c46d0645847de06d
Davide Bellettini <davide.bellettini@gmail.com>
2013-01-30 06:34:13 +0100

    /etc/puppet/modules directory check

--- vagrant/bash/init.sh
+++ vagrant/bash/init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+PUPPET_MODULES=/etc/puppet/modules
+
 aptitude update
 
 if [ `which git | wc -l` -eq 0 ]; then
     aptitude install git --assume-yes
+fi
+
+if [ ! -d $PUPPET_MODULES ]; then
+    mkdir -p $PUPPET_MODULES
 fi
 
 function puppet_install {
