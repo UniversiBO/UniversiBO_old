d32e6950f13a41578a8316705d2657349c354e7b
Davide Bellettini <davide.bellettini@gmail.com>
2013-02-11 22:08:54 +0100

    CS fixing

--- src/Universibo/Bundle/WebsiteBundle/Controller/CommonController.php
+++ src/Universibo/Bundle/WebsiteBundle/Controller/CommonController.php
@@ -31,12 +31,12 @@ class CommonController extends Controller
     public function alertAction()
     {
         $message = $this->container->getParameter('alert_message');
-        
+
         $body = empty($message) ? '' : '<div id="alert">' . $message .'</div>';
-        
+
         return new Response($body);
     }
-    
+
     /**
      * @return Response
      */
