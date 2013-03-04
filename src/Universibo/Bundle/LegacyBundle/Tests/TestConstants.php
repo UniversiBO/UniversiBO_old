46c60041ef1d0537966b88c3c6e95102363b02ff
Davide Bellettini <davide.bellettini@gmail.com>
2013-03-05 00:36:36 +0100

    Updating test constants

--- src/Universibo/Bundle/LegacyBundle/Tests/TestConstants.php
+++ src/Universibo/Bundle/LegacyBundle/Tests/TestConstants.php
@@ -3,12 +3,12 @@ namespace Universibo\Bundle\LegacyBundle\Tests;
 
 class TestConstants
 {
-    const ADMIN_USERNAME = 'fgiardini';
-    const STUDENT_USERNAME = 'Dece';
-    const PROFESSOR_USERNAME = 'edenti';
-    const STAFF_USERNAME = 'maurizio.zani';
-    const COLLABORATOR_USERNAME = 'ossistyl';
-    const TUTOR_USERNAME = 'dtiles';
+    const ADMIN_USERNAME = 'admin';
+    const STUDENT_USERNAME = 'student';
+    const PROFESSOR_USERNAME = 'professor';
+    const STAFF_USERNAME = 'staff';
+    const COLLABORATOR_USERNAME = 'moderator';
+    const TUTOR_USERNAME = 'tutor';
     const DUMMY_PASSWORD = 'padrino';
     const DUMMY_PASSWORD2 = 'madrina';
     const SELENIUM_URL = 'http://www.universibo.dev/app_dev.php';
