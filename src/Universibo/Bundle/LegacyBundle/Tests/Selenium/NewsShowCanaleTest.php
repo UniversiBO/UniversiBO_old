8197eea1b05d1d2d68c1ebe844eaa32e30ec0aab
Davide Bellettini <davide.bellettini@gmail.com>
2013-03-05 21:09:25 +0100

    Making NewsShowCanaleTest work with new test db

--- src/Universibo/Bundle/LegacyBundle/Tests/Selenium/NewsShowCanaleTest.php
+++ src/Universibo/Bundle/LegacyBundle/Tests/Selenium/NewsShowCanaleTest.php
@@ -13,20 +13,20 @@ class NewsShowCanaleTest extends UniversiBOSeleniumTestCase
     public function testNotLogged()
     {
         $this->logout();
-        $this->openPrefix('/news/canale/1?inizio=0&qta=10');
+        $this->openPrefix('/news/canale/2?inizio=0&qta=10');
         $this->assertNotSentence('Scrivi nuova notizia');
     }
 
     public function testLogged()
     {
         $this->login(TestConstants::ADMIN_USERNAME);
-        $this->openPrefix('/news/canale/1?inizio=0&qta=10');
+        $this->openPrefix('/news/canale/2?inizio=0&qta=10');
         $this->assertSentences(array('News','Scrivi nuova notizia'));
     }
 
     public function testPermalink()
     {
-        $this->openPrefix('/news/canale/1?inizio=0&qta=10');
+        $this->openPrefix('/news/canale/2?inizio=0&qta=10');
         $this->clickAndWait('link=permalink');
         $this->assertFalse($this->isTextPresent('Error'), 'Should NOT display an error message');
     }
