5ccb6f9e8b981e297265413d666b33d3d38c180f
Davide Bellettini <davide.bellettini@gmail.com>
2013-03-08 19:09:38 +0100

    Stubbing behat for dashboard

---
+++ src/Universibo/Bundle/DashboardBundle/Features/Context/FeatureContext.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Universibo\Bundle\DashboardBundle\Features\Context;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+use Behat\Symfony2Extension\Context\KernelAwareInterface;
+use Behat\MinkExtension\Context\MinkContext;
+
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+/**
+ * Feature context.
+ */
+class FeatureContext extends MinkContext
+                  implements KernelAwareInterface
+{
+    private $kernel;
+    private $parameters;
+
+    /**
+     * Initializes context with parameters from behat.yml.
+     *
+     * @param array $parameters
+     */
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Sets HttpKernel instance.
+     * This method will be automatically called by Symfony2Extension ContextInitializer.
+     *
+     * @param KernelInterface $kernel
+     */
+    public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+}
