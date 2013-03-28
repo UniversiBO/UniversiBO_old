569fbb97e23855a21a77546d2a0b21b80191c809
Davide Bellettini <davide.bellettini@gmail.com>
2013-03-28 19:31:56 +0100

    Controller is complete #278

--- src/Universibo/Bundle/MainBundle/Controller/SchoolController.php
+++ src/Universibo/Bundle/MainBundle/Controller/SchoolController.php
@@ -28,7 +28,28 @@ class SchoolController extends Controller
         $school = $channel->getSchool();
 
         $courseRepo = $this->get('universibo_legacy.repository.cdl');
-        $courses = $courseRepo->findBySchool($school);
+        $foundCourses = $courseRepo->findBySchool($school);
+
+        $translation = [
+            1 => 'CORSI DI LAUREA TRIENNALE',
+            2 => 'CORSI DI LAUREA SPECIALISTICA',
+            3 => 'CORSI DI LAUREA VECCHIO ORDINAMENTO',
+            4 => 'CORSI DI LAUREA TRIENNALE NUOVO ORDINAMENTO',
+            5 => 'CORSI DI LAUREA MAGISTRALE',
+            6 => 'CORSI DI LAUREA MAGISTRALE A CICLO UNICO',
+            7 => 'CORSI DI LAUREA SPECIALISTICA A CICLO UNICO',
+        ];
+
+        $courses = array();
+        foreach ($foundCourses as $course) {
+            $courses[$translation[$course->getCategoriaCdl()]][] = $course;
+        }
+
+        foreach ($courses as &$category) {
+            uasort($category, function($a, $b){
+                return strcasecmp($a->getNome(), $b->getNome());
+            });
+        }
 
         return ['school' => $school, 'courses' => $courses];
     }
