f6e9f1d092608f9b992baa0804fb366768a51545
Davide Bellettini <davide.bellettini@gmail.com>
2013-11-11 13:28:46 +0100

    Fixing vendors

--- app/autoload.php
+++ app/autoload.php
@@ -1,27 +1,16 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Symfony\Component\ClassLoader\ApcClassLoader;
+use Composer\Autoload\ClassLoader;
 
 define('PHP_EXTENSION', '.php');
 define('UNIVERSIBO_ROOT', realpath(__DIR__.'/..'));
 
-if (!$loader = include __DIR__.'/../vendor/autoload.php') {
-    $nl = PHP_SAPI === 'cli' ? PHP_EOL : '<br />';
-    echo "$nl$nl";
-    die('You must set up the project dependencies.'.$nl.
-        'Run the following commands in '.dirname(__DIR__).':'.$nl.$nl.
-        'curl -s http://getcomposer.org/installer | php'.$nl.
-        'php composer.phar install'.$nl);
-}
-
-// intl
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
-}
+/**
+ * @var ClassLoader $loader
+ */
+$loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
 
-$loader->register(true);
+return $loader;
